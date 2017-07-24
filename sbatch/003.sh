6e39cf0587d3186625f2bc65a1d40a6abdbc561a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-24 14:29:51 -0400

    write 003.sh

---
+++ sbatch/003.sh
@@ -0,0 +1,5 @@
+# This shell script executes the Slurm job for creating HDF5 files gathering
+# all WAV clips belonging to a unit-augmentation pair, along with some
+# metadata in GPS coordinates and UTC starting time of each recording.
+
+sbatch 003.sbatch
