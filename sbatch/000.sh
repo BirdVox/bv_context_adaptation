882c8a86302886472401a35cdf9e8e73806edbb8
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-24 14:30:54 -0400

    update 000.sh

--- sbatch/000.sh
+++ sbatch/000.sh
@@ -1,9 +1,9 @@
 # This shell script executes all 6 Slurm jobs for generating the BirdVox-70k
 # dataset. Every job corresponds to a different recording unit.
 
-sbatch 000-0.sbatch
-sbatch 000-1.sbatch
-sbatch 000-2.sbatch
-sbatch 000-3.sbatch
-sbatch 000-4.sbatch
-sbatch 000-5.sbatch
+sbatch 000-01.sbatch
+sbatch 000-02.sbatch
+sbatch 000-03.sbatch
+sbatch 000-05.sbatch
+sbatch 000-07.sbatch
+sbatch 000-10.sbatch
