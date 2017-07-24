9c910201c387f210941d558cc8cdd9d0874382fe
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-24 17:34:12 -0400

    update 002

--- src/002_augment_BirdVox-70k.py
+++ src/002_augment_BirdVox-70k.py
@@ -11,12 +11,11 @@ import localmodule
 
 # Define constants
 data_dir = localmodule.get_data_dir()
-args = sys.argv[1:]
-aug_str = args[0]
-unit_id = int(args[1])
 units = localmodule.get_units()
-unit = units[unit_id]
-n_units = len(units)
+args = sys.argv[1:]
+unit = int(args[0])
+aug_str = args[1]
+other_units = units.remove(unit)
 
 # Print header
 start_time = int(time.time())
@@ -28,15 +27,17 @@ print("")
 clips_dir = os.path.join(data_dir, "BirdVox-70k")
 if not os.path.exists(clips_dir):
     os.makedirs(clips_dir)
+original_clips_dir = os.path.join(clips_dir, "original")
 aug_clips_dir = os.path.join(clips_dir, aug_str)
 if not os.path.exists(aug_clips_dir):
     os.makedirs(aug_clips_dir)
 
 # Create directory corresponding to the recording unit
 unit_str = "unit" + str(unit).zfill(2)
-unit_dir = os.path.join(aug_clips_dir, unit_str)
-if not os.path.exists(unit_dir):
-    os.makedirs(unit_dir)
+in_unit_dir = os.path.join(original_clips_dir, unit_str)
+out_unit_dir = os.path.join(aug_clips_dir, unit_str)
+if not os.path.exists(out_unit_dir):
+    os.makedirs(out_unit_dir)
 
 
 
