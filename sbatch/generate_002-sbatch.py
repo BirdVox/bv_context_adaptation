2e22cf63af084626625e33437a2c011e859cd249
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 19:56:35 -0400

    update generate_002-sbatch.py

--- sbatch/generate_002-sbatch.py
+++ sbatch/generate_002-sbatch.py
@@ -19,11 +19,10 @@ for aug_str in augmentations:
         instance_str = str(instance_id)
 
         # Loop over recording units.
-        for unit in units:
-            unit_str = "unit" + str(unit).zfill(2)
+        for unit_str in units:
             job_name = "_".join(["002", aug_str, instance_str, unit_str])
             file_name = job_name + ".sbatch"
-            script_list = [script_path, aug_str, instance_str, str(unit).zfill(2)]
+            script_list = [script_path, aug_str, instance_str, unit_str]
             script_path_with_args = " ".join(script_list)
             with open(file_name, "w") as f:
                 f.write("#!/bin/bash\n")
@@ -40,5 +39,5 @@ for aug_str in augmentations:
                 f.write("\n")
                 f.write("# The first argument is the name of the augmentation.\n")
                 f.write("# The second argument is the instance ID of the augmentation.\n")
-                f.write("# The third argument is the ID of the recording unit.\n")
+                f.write("# The third argument is the name of the recording unit.\n")
                 f.write("python " + script_path_with_args)
