64456eed488bec23cfd609315d2644ebde3d07df
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 18:21:11 -0400

    update generate_000-sbatch to be cross-platform

--- sbatch/generate_000-sbatch.py
+++ sbatch/generate_000-sbatch.py
@@ -5,14 +5,15 @@ import localmodule
 
 # Define constants
 units = localmodule.get_units()
-model_name = "000_generate_BirdVox-70k_wav.py"
+script_name = "000_generate_BirdVox-70k_wav.py"
+script_path = os.path.join("..", "src", script_name)
 
 # Loop over recording units
 for unit in units:
     unit_str = "unit" + str(unit).zfill(2)
     job_name = "_".join(["000", unit_str])
     file_name = job_name + ".sbatch"
-    model_name_with_args = " ".join([model_name, str(unit).zfill(2)])
+    script_path_with_args = " ".join([script_path, str(unit).zfill(2)])
     with open(file_name, "w") as f:
         f.write("#!/bin/bash\n")
         f.write("\n")
@@ -28,4 +29,4 @@ for unit in units:
         f.write("\n")
         f.write("# The integer passed to the Python script 000_generate_BirdVox-70k_wav.py\n")
         f.write("# corresponds to the ID of the recording unit in the list [1, 2, 3, 5, 7, 10].\n")
-        f.write("python ../src/" + model_name_with_args)
+        f.write("python ../src/" + script_path_with_args)
