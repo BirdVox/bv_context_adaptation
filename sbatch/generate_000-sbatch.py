52e4e71b3bc7a9f91f42e4c6d87058648a067259
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 18:22:01 -0400

    minor fix in generate_000-sbatch.py

--- sbatch/generate_000-sbatch.py
+++ sbatch/generate_000-sbatch.py
@@ -29,4 +29,4 @@ for unit in units:
         f.write("\n")
         f.write("# The integer passed to the Python script 000_generate_BirdVox-70k_wav.py\n")
         f.write("# corresponds to the ID of the recording unit in the list [1, 2, 3, 5, 7, 10].\n")
-        f.write("python ../src/" + script_path_with_args)
+        f.write("python " + script_path_with_args)
