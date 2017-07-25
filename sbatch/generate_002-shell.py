633420a9b42fc9aecd23f277e2e02ca4f466055a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 18:06:00 -0400

    refactor generate_002-shell.py

--- sbatch/generate_002-shell.py
+++ sbatch/generate_002-shell.py
@@ -3,30 +3,40 @@ import sys
 sys.path.append("../src")
 import localmodule
 
+# Define constants.
 units = localmodule.get_units()
 augmentations = localmodule.get_augmentations()
 del augmentations["original"]
 file_path = "002.sh"
 
+
+# Open shell file.
 with open(file_path, "w") as f:
+    # Print header.
     f.write("# This shell script executes the Slurm jobs for data augmentation.\n")
     f.write("\n")
-    # Loop over recording units
-    for unit in units:
-        unit_str = str(unit).zfill(2)
-        # Loop over augmentations
-        for aug_str in augmentations:
-            n_aug_instances = augmentations[aug_str]
-            # Loop over instances
-            for aug_instance_id in range(n_aug_instances):
-                aug_instance_str = str(aug_instance_id)
-                job_name = "_".join(["002", unit_str, aug_str, aug_instance_str])
+
+    # Loop over augmentations.
+    for aug_str in augmentations:
+
+        # Loop over instances.
+        n_instances = augmentations[aug_str]
+        for instance_id in range(n_instances):
+            instance_str = str(instance_id)
+
+            # Loop over recording units
+            for unit in units:
+                unit_str = str(unit).zfill(2)
+                # Define job name.
+                job_name = "_".join(["002", aug_str, instance_str, unit_str])
                 sbatch_str = "sbatch " + job_name + ".sbatch"
+                # Write SBATCH command to file
                 f.write(sbatch_str + "\n")
             f.write("\n")
         f.write("\n")
 
-# Grant permission to execute the file
+
+# Grant permission to execute the shell file.
 # https://stackoverflow.com/a/30463972
 mode = os.stat(file_path).st_mode
 mode |= (mode & 0o444) >> 2
