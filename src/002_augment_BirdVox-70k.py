331a9f647950e2366759e1443a49e82786ffa59f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 13:43:53 -0400

    update comments in 002

--- src/002_augment_BirdVox-70k.py
+++ src/002_augment_BirdVox-70k.py
@@ -10,7 +10,7 @@ import time
 import localmodule
 
 
-# Define constants
+# Define constants.
 data_dir = localmodule.get_data_dir()
 units = localmodule.get_units()
 args = sys.argv[1:]
@@ -18,7 +18,8 @@ unit = int(args[0])
 aug_str = args[1]
 aug_id = int(args[2])
 
-# Print header
+
+# Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start")
 print("Augmenting BirdVox-70k clips for unit " + str(unit).zfill(2))
@@ -28,7 +29,8 @@ print("muda version: {:s}'.format(muda.__version__)")
 print("numpy version: {:s}'.format(numpy.__version__)")
 print("")
 
-# Create directory for augmented clips
+
+# Create directory for augmented clips.
 clips_dir = os.path.join(data_dir, "BirdVox-70k")
 if not os.path.exists(clips_dir):
     os.makedirs(clips_dir)
@@ -37,28 +39,32 @@ aug_clips_dir = os.path.join(clips_dir, aug_str)
 if not os.path.exists(aug_clips_dir):
     os.makedirs(aug_clips_dir)
 
-# Create directory corresponding to the recording unit
+
+# Create directory corresponding to the recording unit.
 unit_str = "unit" + str(unit).zfill(2)
 in_unit_dir = os.path.join(original_clips_dir, unit_str)
 out_unit_dir = os.path.join(aug_clips_dir, unit_str)
 if not os.path.exists(out_unit_dir):
     os.makedirs(out_unit_dir)
 
-# Define deformers
+
+# Define deformers.
 if aug_str[:5] == "noise":
-    # Background noise deformers
+    # Background noise deformers.
     noise_unit = int(aug_str[10:])
+
     # For each recording unit, we create a deformer which adds a negative
     # example (i.e. containing no flight call) to the current clip, weighted
     # by a randomized amplitude factor ranging between 0.1 and 0.5.
     # This does not change the label because
     # negative + negative = negative
     # and
-    # positive + negative = positive
+    # positive + negative = positive.
     noise_unit_str = str(unit).zfill(2)
     noise_unit_dir = os.path.join(original_clips_dir, unit_str)
+
     # This regular expression selects only negative, non-augmented examples
-    # for background noise
+    # for background noise.
     regexp = "*_0_original.wav"
     names = sorted(glob.glob(os.path.join(noise_unit_dir, regexp)))
     noise_paths = [os.path.join(noise_unit_dir, name) for name in names]
@@ -79,10 +85,19 @@ elif aug_str == "stretch":
         n_samples=1, location=0.0, scale=0.1)
 
 
-wav_names = sorted(glob.glob(os.path.join(in_unit_dir)))
+# Extract WAV and JAMS files corresponding to recording unit.
+wav_paths = sorted(glob.glob(os.path.join(in_unit_dir, "*.wav")))
+jam_paths = sorted(glob.glob(os.path.join(in_unit_dir, "*.jams")))
 
 
-# Print elapsed time
+# Loop over examples.
+for (wav_path, jam_path) in zip(wav_paths, jam_paths):
+    jam_original = muda.load_jam_audio(ja_path, wav_path)
+    jam_transformer = transformer.transform(jam_original)
+
+
+
+# Print elapsed time.
 print(str(datetime.datetime.now()) + " Finish")
 elapsed_time = time.time() - int(start_time)
 elapsed_hours = int(elapsed_time / (60 * 60))
