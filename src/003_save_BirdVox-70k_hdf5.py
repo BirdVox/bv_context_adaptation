a82b266f9d4cb066d194b78f9ee9052c907daed4
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 17:59:06 -0400

    continue writing 003

--- src/003_save_BirdVox-70k_hdf5.py
+++ src/003_save_BirdVox-70k_hdf5.py
@@ -3,6 +3,7 @@ import os
 
 # Define constants.
 data_dir = localmodule.get_data_dir()
+BirdVox_wav_dir = os.path.join(data_dir, "BirdVox-70k_wav")
 units = localmodule.get_units()
 augmentations = localmodule.get_augmentations()
 
@@ -14,16 +15,17 @@ print("Saving BirdVox-70k data into HDF5 containers.")
 print("h5py version: {:s}'.format(h5py.__version__)")
 print("")
 
+# Loop over augmentations.
+for aug_str in augmentations:
+    n_instances = augmentations[aug_str]
 
-# Loop over recording units.
-for unit in units:
-    unit_str = "unit" + str(unit).zfill(2)
-    in_unit_dir = os.path.join(data_dir, "BirdVox-70k", unit_str)
+    # Loop over instances.
+    for instance_id in range(n_instances):
+        instance_str = str(instance_id)
+        instanced_aug_str = "-".join(aug_str, instance_str)
+        instanced_aug_dir = os.path.join(BirdVox_wav_dir, instanced_aug_str)
 
-    # Loop over augmentations.
-    for aug_str in augmentations:
-        n_instances = augmentations[aug_str]
-
-        # Loop over instances.
-        for instance_id in range(n_instances):
-            instance_str = str(instance_id)
+        # Loop over recording units.
+        for unit in units:
+            unit_str = "unit" + str(unit).zfill(2)
+            in_unit_dir = os.path.join(instanced_aug_dir, unit_str)
