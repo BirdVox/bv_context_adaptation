70a10037aacb46ba18ae93e8ac6ee14fb71c4dd2
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 17:58:38 -0400

    write generate_000-sbatch.py

---
+++ sbatch/generate_000-sbatch.py
@@ -0,0 +1,31 @@
+import os
+import sys
+sys.path.append("../src")
+import localmodule
+
+# Define constants
+units = localmodule.get_units()
+model_name = "000_generate_BirdVox-70k_wav.py"
+
+# Loop over recording units
+for unit in units:
+    unit_str = str(unit).zfill(2)
+    job_name = "_".join("000", unit_str)
+    file_name = job_name + ".sbatch"
+    model_name_with_args = " ".join(model_name, unit_str)
+    with open(file_name, "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("\n")
+        f.write("#BATCH --job-name=" + job_name + "\n")
+        f.write("#SBATCH --nodes=1\n")
+        f.write("#SBATCH --tasks-per-node=1\n")
+        f.write("#SBATCH --cpus-per-task=1\n")
+        f.write("#SBATCH --time=0:30:00\n")
+        f.write("#SBATCH --mem=4GB\n")
+        f.write("#SBATCH --output=slurm_" + job_name + "_%j.out\n")
+        f.write("\n")
+        f.write("module purge\n")
+        f.write("\n")
+        f.write("# The integer passed to the Python script 000_generate_BirdVox-70k_wav.py")
+        f.write("# corresponds to the ID of the recording unit in the list [1, 2, 3, 5, 7, 10].")
+        f.write("python ../src/" + model_name_with_args)
