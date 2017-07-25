b4d9c7f27b89e90962527d14a1910305a9c5104e
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 19:28:23 -0400

    update comments in 001.sh

--- sbatch/001.sh
+++ sbatch/001.sh
@@ -1,5 +1,5 @@
 # This shell script executes the Slurm job for generating JAMS (JSON Annotated
-# Music Specification) metadata for all 6 recording units in BirdVox-70k.
+# Music Specification) metadata for all recording units.
 # This is needed as a prior step to data augmentation.
 
 sbatch 001.sbatch
