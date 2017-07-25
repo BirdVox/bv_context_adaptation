9b7537200d5b950d37b1f4bb69a3b627180ce312
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 11:39:44 -0400

    update get_augmentations

--- src/localmodule.py
+++ src/localmodule.py
@@ -1,8 +1,13 @@
 def get_augmentations():
-    return ["noise", "original", "pitch", "stretch"]
+    units = get_units()
+    augmentations = ["noise-unit" + str(unit).zfill(2) for unit in units] +
+        ["original", "pitch", "stretch"]
+    return augmentations
+
 
 def get_data_dir():
 	return "/scratch/vl1019/spl2017_data"
+
 
 def get_units():
     return [1, 2, 3, 5, 7, 10]
