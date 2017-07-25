7e9628bdc5f0f4ce8e789c0901d40260f54814c9
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-25 13:43:45 -0400

    update comments in 001

--- src/001_generate_BirdVox-70k_jams.py
+++ src/001_generate_BirdVox-70k_jams.py
@@ -9,7 +9,7 @@ import time
 import paths
 
 
-# Define constants
+# Define constants.
 data_dir = localmodule.get_data_dir()
 BirdVox_dir = os.path.join(data_dir, "BirdVox-70k")
 original_BirdVox_dir = os.path.join(BirdVox_dir, "original")
@@ -18,7 +18,7 @@ n_units = len(units)
 clip_duration = 0.5
 
 
-# Print header
+# Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start")
 print("Generating BirdVox-70k JAMS metadata")
@@ -29,7 +29,7 @@ print('librosa version: {:s}'.format(librosa.__version__))
 print("")
 
 
-# Loop across recording units
+# Loop across recording units.
 for unit_id in range(n_units):
     unit = units[unit_id]
     unit_str = "unit" + str(unit).zfill(2)
@@ -38,40 +38,40 @@ for unit_id in range(n_units):
     names = sorted(names)
     n_names = len(names)
 
-    # Loop across names
+    # Loop across names.
     for name_id in range(n_names):
         name = names[name_id]
 
-        # Initialize JAMS metadata file
+        # Initialize JAMS metadata file.
         jam = jams.JAMS()
 
-        # Create annotation
+        # Create annotation.
         ann = jams.Annotation('tag_open')
         ann.duration = clip_duration
 
-        # Add tag with snippet sound class
+        # Add tag with snippet sound class.
         ann.append(time=0, duration=clip_duration, value=name[23], confidence=1)
 
-        # Fill file metadata
+        # Fill file metadata.
         jam.file_metadata.title = name[7:16]
         jam.file_metadata.release = '1.0'
         jam.file_metadata.duration = clip_duration
         jam.file_metadata.artist = name[:6]
 
-        # Fill annotation metadata
+        # Fill annotation metadata.
         ann.annotation_metadata.version = '1.0'
         ann.annotation_metadata.corpus = 'BirdVox-70k'
 
-        # Add annotation
+        # Add annotation.
         jam.annotations.append(ann)
 
-        # Export JAM file
+        # Export JAM file.
         jam_name = name[:-3] + "jams"
         out_path = os.path.join(unit_dir, jam_name)
         jam.save(out_path)
 
 
-# Print elapsed time
+# Print elapsed time.
 print(str(datetime.datetime.now()) + " Finish")
 elapsed_time = time.time() - int(start_time)
 elapsed_hours = int(elapsed_time / (60 * 60))
