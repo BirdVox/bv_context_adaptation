e358721871173b5d20816f363cda7fe314865980
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-28 16:02:32 -0400

    write split_units

--- src/localmodule.py
+++ src/localmodule.py
@@ -45,3 +45,19 @@ def rsync():
     command_words = ["rsync"] + flags + [data_dir, archive_dir]
     command_str = " ".join(command_words)
     os.command(command_str)
+
+
+# We perform leave-one-unit-out cross-validation with 2 units for validation
+def split_units():
+    units = localmodule.get_units()
+    n_units = len(units)
+    folds = []
+    for fold_id in range(n_units):
+        test_units = [units[fold_id]]
+        train_units = []
+        for unit_id in range(fold_id+1, fold_id+n_units-2):
+            train_units.append(units[np.mod(unit_id, n_units)])
+        val_units = [
+            units[np.mod(fold_id-2, n_units)],
+            units[np.mod(fold_id-1, n_units)]]
+        folds.append([test_units, train_units, val_units])
