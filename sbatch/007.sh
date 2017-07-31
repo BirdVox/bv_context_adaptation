82adda6b32c7334fc6f19f8ae906f52e6dd541ec
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-31 19:43:10 -0400

    generate 007.sh

---
+++ sbatch/007.sh
@@ -0,0 +1,8 @@
+# This shell script executes Slurm jobs for evaluating the SKM model on all units.
+
+sbatch 007_unit01.sbatch
+sbatch 007_unit02.sbatch
+sbatch 007_unit03.sbatch
+sbatch 007_unit05.sbatch
+sbatch 007_unit07.sbatch
+sbatch 007_unit10.sbatch
