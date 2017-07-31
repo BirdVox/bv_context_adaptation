6173908fd8c88a79d56e9ef3acb1d62368f84522
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-31 11:41:49 -0400

    rename split_units to fold_units

--- src/localmodule.py
+++ src/localmodule.py
@@ -55,6 +55,23 @@ def bresenham(input_length, output_length, n_layers):
                 pool_list.append(floor_pooling)
 
 
+# We perform leave-one-unit-out cross-validation with 2 units for validation
+def fold_units():
+    units = get_units()
+    n_units = len(units)
+    folds = []
+    for fold_id in range(n_units):
+        test_units = [units[fold_id]]
+        train_units = []
+        for unit_id in range(fold_id+1, fold_id+n_units-2):
+            train_units.append(units[np.mod(unit_id, n_units)])
+        val_units = [
+            units[np.mod(fold_id-2, n_units)],
+            units[np.mod(fold_id-1, n_units)]]
+        folds.append([test_units, train_units, val_units])
+    return folds
+
+
 def get_augmentations():
     units = get_units()
     augmentations = {("noise-" + unit_str): 4 for unit_str in units}
@@ -99,20 +116,3 @@ def rsync():
     command_words = ["rsync"] + flags + [data_dir, archive_dir]
     command_str = " ".join(command_words)
     os.command(command_str)
-
-
-# We perform leave-one-unit-out cross-validation with 2 units for validation
-def split_units():
-    units = get_units()
-    n_units = len(units)
-    folds = []
-    for fold_id in range(n_units):
-        test_units = [units[fold_id]]
-        train_units = []
-        for unit_id in range(fold_id+1, fold_id+n_units-2):
-            train_units.append(units[np.mod(unit_id, n_units)])
-        val_units = [
-            units[np.mod(fold_id-2, n_units)],
-            units[np.mod(fold_id-1, n_units)]]
-        folds.append([test_units, train_units, val_units])
-    return folds
