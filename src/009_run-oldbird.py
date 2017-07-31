7106c3e94624145778c732ee2200bcc87656a640
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-31 12:49:25 -0400

    typo

--- src/009_run-oldbird.py
+++ src/009_run-oldbird.py
@@ -40,7 +40,7 @@ chunk_padding_length = int(np.round(chunk_padding_duration * sample_rate))
 # Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start.")
-print("Running Old Bird detectors (Thrush and Tseep) on " dataset_name +
+print("Running Old Bird detectors (Thrush and Tseep) on " + dataset_name +
     ", " + unit_str + ".")
 print('numpy version: {:s}'.format(np.__version__))
 print('scipy version: {:s}'.format(scipy.__version__))
