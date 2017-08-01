80c91c6b55398d9f4f8f790c0b501745b995a4bb
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-31 20:22:20 -0400

    bugfix 007

--- src/007_evaluate-skm.py
+++ src/007_evaluate-skm.py
@@ -20,9 +20,9 @@ units = localmodule.get_units()
 n_thresholds = 5 #100
 negative_labels = localmodule.get_negative_labels()
 args = sys.argv[1:]
-unit_str = args[0]
-tolerance_ms = int(args[1])
+tolerance_ms = int(args[0])
 tolerance = tolerance_ms / 1000
+unit_str = args[1]
 
 
 # Print header.
