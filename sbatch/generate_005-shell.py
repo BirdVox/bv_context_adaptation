5e86717fe02eb525ea3d1a5ccd70f816cf458d4d
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 14:35:43 -0400

    write generate_005-shell

---
+++ sbatch/generate_005-shell.py
@@ -0,0 +1,47 @@
+import os
+import sys
+sys.path.append("../src")
+import localmodule
+
+# Define constants.
+units = localmodule.get_units()
+augmentations = localmodule.get_augmentations()
+file_path = "005.sh"
+
+
+# Open shell file.
+with open(file_path, "w") as f:
+    # Print header.
+    f.write("# This shell script executes the Slurm jobs for computing log-mel-spectrograms.\n")
+    f.write("\n")
+
+    # Loop over augmentations.
+    for aug_str in augmentations:
+
+        # Loop over instances.
+        n_instances = augmentations[aug_str]
+        for instance_id in range(n_instances):
+            instance_str = str(instance_id)
+
+            if aug_str == "original":
+                instanced_aug_str = aug_str
+            else:
+                instanced_aug_str = "_".join([aug_str, instance_str])
+
+            # Loop over recording units.
+            for unit_str in units:
+                # Define job name.
+                job_name = "_".join(["005", instanced_aug_str, unit_str])
+                sbatch_str = "sbatch " + job_name + ".sbatch"
+
+                # Write SBATCH command to shell file.
+                f.write(sbatch_str + "\n")
+            f.write("\n")
+        f.write("\n")
+
+
+# Grant permission to execute the shell file.
+# https://stackoverflow.com/a/30463972
+mode = os.stat(file_path).st_mode
+mode |= (mode & 0o444) >> 2
+os.chmod(file_path, mode)
