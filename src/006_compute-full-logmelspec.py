ca8e684f5e546996ccf9e24443a5208c6bdcae61
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 19:49:07 -0400

    update 006

--- src/006_compute-full-logmelspec.py
+++ src/006_compute-full-logmelspec.py
@@ -3,12 +3,11 @@ import h5py
 import librosa
 import os
 import pandas as pd
+import soundfile as sf
 import sys
 import time
 
-sys.path.append('../src') #                                    DISABLE
 import localmodule
-
 
 
 # Define constants.
@@ -18,6 +17,7 @@ dataset_name = localmodule.get_dataset_name()
 args = ["unit01"] #                                             DISABLE
 unit_str = args[0]
 logmelspec_settings = localmodule.get_logmelspec_settings()
+n_clips_per_chunk = 1000
 
 
 # Print header.
@@ -29,6 +29,7 @@ print("")
 print("h5py version: {:s}".format(h5py.__version__))
 print("librosa version: {:s}".format(librosa.__version__))
 print("pandas version: {:s}".format(pd.__version__))
+print("soundfile version: {:s}".format(sf.__version__))
 print("")
 
 
@@ -71,6 +72,93 @@ settings_group["n_mels"] = logmelspec_settings["n_mels"]
 settings_group["sr"] = logmelspec_settings["sr"]
 settings_group["win_length"] = logmelspec_settings["win_length"]
 settings_group["window"] = logmelspec_settings["window"]
+
+
+# Start HDF5 group for log-mel-spectrograms (logmelspec).
+lms_group = out_file.create_group("logmelspec")
+
+
+# Open full audio file as FLAC.
+recordings_name = "_".join([dataset_name, "full-audio"])
+recordings_dir = os.path.join(data_dir, recordings_name)
+recording_name = unit_str + ".flac"
+recording_path = os.path.join(recordings_dir, recording_name)
+full_audio = sf.SoundFile(recording_path)
+full_audio_length = len(full_audio)
+
+
+# Define a time sample for the middle of every audio clip.
+sample_rate = localmodule.get_sample_rate()
+lms_sample_rate = logmelspec_settings["sr"]
+lms_hop_length = logmelspec_settings["hop_length"]
+sample_float_step = 64 * lms_hop_length * sample_rate / lms_sample_rate
+clip_length = int(np.round(0.5 * sample_rate))
+half_clip_length = int(np.round(0.25 * sample_rate))
+sample_start = half_clip_length
+sample_stop = full_audio_length - half_clip_length
+sample_range = np.arange(sample_start, sample_stop, sample_float_step)
+sample_range = np.round(sample_range).astype('int')
+n_clips = len(sample_range)
+n_chunks = int(np.ceil(n_clips / n_clips_per_chunk))
+samples_per_hop = lms_hop_length * sample_rate / lms_sample_rate
+
+
+# Loop over chunks.
+for chunk_id in range(n_chunks):
+    # Load audio chunk.
+    first_clip_id = chunk_id * n_clips_per_chunk
+    last_clip_id = min((chunk_id+1) * n_clips_per_chunk, n_clips)
+    chunk_range = range(first_clip_id, last_clip_id)
+    chunk_sample_range = sample_range[chunk_range]
+    chunk_start = chunk_sample_range[0] - half_clip_length
+    chunk_stop = chunk_sample_range[-1] + half_clip_length
+    full_audio.seek(chunk_start)
+    chunk_waveform = full_audio.read(chunk_stop-chunk_start)
+
+    # Resample to 22050 Hz.
+    chunk_waveform = librosa.resample(
+        chunk_waveform, sample_rate, logmelspec_settings["sr"])
+
+    # Compute Short-Term Fourier Transform (STFT).
+    stft = librosa.stft(
+        chunk_waveform,
+        n_fft=logmelspec_settings["n_fft"],
+        win_length=logmelspec_settings["win_length"],
+        hop_length=logmelspec_settings["hop_length"],
+        window=logmelspec_settings["window"])
+
+    # Compute squared magnitude coefficients.
+    abs2_stft = (stft.real*stft.real) + (stft.imag*stft.imag)
+
+    # Gather frequency bins according to the Mel scale.
+    melspec = librosa.feature.melspectrogram(
+        y=None,
+        S=abs2_stft,
+        sr=logmelspec_settings["sr"],
+        n_fft=logmelspec_settings["n_fft"],
+        n_mels=logmelspec_settings["n_mels"],
+        htk=True,
+        fmin=logmelspec_settings["fmin"],
+        fmax=logmelspec_settings["fmax"])
+
+    # Apply pointwise base-10 logarithm.
+    logmelspec = 0.5 * librosa.logamplitude(melspec, ref=1.0)
+
+    # Convert to single floating-point precision.
+    chunk_logmelspec = logmelspec.astype('float32')
+
+    # Loop over audio clips.
+    for clip_id in range(n_clips_per_chunk):
+        # Extract logmelspec clip in chunk
+        mid_sample = chunk_sample_range[clip_id]
+        mid_rel_sample = mid_sample - chunk_start
+        mid_hop = int(np.round(mid_rel_sample / samples_per_hop))
+        start_hop = mid_hop - int(np.round(half_clip_length / samples_per_hop))
+        stop_hop = mid_hop + int(np.round(half_clip_length / samples_per_hop))
+        clip_range = range(start_hop, stop_hop)
+        clip_logmelspec = chunk_logmelspec[:, clip_range]
+
+        clip_name = str(mid_sample).zfill(9)
 
 
 # Print elapsed time.
