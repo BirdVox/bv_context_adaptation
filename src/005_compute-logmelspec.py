aabed3df3c485b7733224bc2d4926a1107c88c8f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 13:45:20 -0400

    bis

--- src/005_compute-logmelspec.py
+++ src/005_compute-logmelspec.py
@@ -15,7 +15,8 @@ sample_rate = localmodule.get_sample_rate()
 args = ["stretch", 0, "unit01"] #                          DISABLE ME
 #args = sys.argv[1:]                                         ENABLE ME
 aug_str = args[0]
-instance_str = str(int(args[1]))
+instance_id = int(args[1])
+instance_str = str(instance_id)
 unit_str = args[2]
 if aug_str == "original":
     instanced_aug_str = aug_str
@@ -40,6 +41,7 @@ print("")
 hdf5_dataset_name = "_".join([dataset_name, "hdf5"])
 hdf5_dir = os.path.join(data_dir, hdf5_dataset_name)
 in_aug_dir = os.path.join(hdf5_dir, aug_str)
+hdf5_name = "_".join([dataset_name, instanced_aug_str, unit_str])
 in_path = os.path.join(in_aug_dir, hdf5_name + ".hdf5")
 in_file = h5py.File(in_path, "r")
 
@@ -50,14 +52,17 @@ logmelspec_dir = os.path.join(data_dir, logmelspec_name)
 os.makedirs(logmelspec_dir, exist_ok=True)
 out_aug_dir = os.path.join(logmelspec_dir, aug_str)
 os.makedirs(out_aug_dir, exist_ok=True)
-hdf5_name = "_".join([dataset_name, instanced_aug_str, unit_str])
 out_path = os.path.join(out_aug_dir, hdf5_name + ".hdf5")
 out_file = h5py.File(out_path)
 
 # Copy over metadata
-out_file["gps_coordinates"] = in_file["gps_coordinates"]
-out_file["sample_rate"] =
-out_file["utc_start_time"] = in_file["utc_start_time"]
+#out_file["gps_coordinates"] = in_file["gps_coordinates"]
+out_file["dataset_name"] = localmodule.get_dataset_name()
+out_file["unit"] = unit_str
+out_file["augmentation"] = aug_str
+out_file["instance"] = instance_id
+out_file["settings"] = logmelspec_settings
+out_file["utc_start_time"] = in_file["utc_start_time"].value
 #waveforms = in_file["waveforms"]
 #durations = [waveforms[key].shape[0] for key in waveforms.keys()]
 
