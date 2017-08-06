a9e682bd357e34fbad099dc7b613d1d828f09867
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 14:37:38 -0400

    generate 005.sh

--- sbatch/005.sh
+++ sbatch/005.sh
@@ -1,4 +1,242 @@
-# This shell script executes the Slurm job for archiving WAV and JAMS files
-# after applying data augmentation.
+# This shell script executes the Slurm jobs for computing log-mel-spectrograms.
 
-sbatch 005.sbatch
+sbatch 005_noise-unit05_0_unit01.sbatch
+sbatch 005_noise-unit05_0_unit02.sbatch
+sbatch 005_noise-unit05_0_unit03.sbatch
+sbatch 005_noise-unit05_0_unit05.sbatch
+sbatch 005_noise-unit05_0_unit07.sbatch
+sbatch 005_noise-unit05_0_unit10.sbatch
+
+sbatch 005_noise-unit05_1_unit01.sbatch
+sbatch 005_noise-unit05_1_unit02.sbatch
+sbatch 005_noise-unit05_1_unit03.sbatch
+sbatch 005_noise-unit05_1_unit05.sbatch
+sbatch 005_noise-unit05_1_unit07.sbatch
+sbatch 005_noise-unit05_1_unit10.sbatch
+
+sbatch 005_noise-unit05_2_unit01.sbatch
+sbatch 005_noise-unit05_2_unit02.sbatch
+sbatch 005_noise-unit05_2_unit03.sbatch
+sbatch 005_noise-unit05_2_unit05.sbatch
+sbatch 005_noise-unit05_2_unit07.sbatch
+sbatch 005_noise-unit05_2_unit10.sbatch
+
+sbatch 005_noise-unit05_3_unit01.sbatch
+sbatch 005_noise-unit05_3_unit02.sbatch
+sbatch 005_noise-unit05_3_unit03.sbatch
+sbatch 005_noise-unit05_3_unit05.sbatch
+sbatch 005_noise-unit05_3_unit07.sbatch
+sbatch 005_noise-unit05_3_unit10.sbatch
+
+
+sbatch 005_noise-unit10_0_unit01.sbatch
+sbatch 005_noise-unit10_0_unit02.sbatch
+sbatch 005_noise-unit10_0_unit03.sbatch
+sbatch 005_noise-unit10_0_unit05.sbatch
+sbatch 005_noise-unit10_0_unit07.sbatch
+sbatch 005_noise-unit10_0_unit10.sbatch
+
+sbatch 005_noise-unit10_1_unit01.sbatch
+sbatch 005_noise-unit10_1_unit02.sbatch
+sbatch 005_noise-unit10_1_unit03.sbatch
+sbatch 005_noise-unit10_1_unit05.sbatch
+sbatch 005_noise-unit10_1_unit07.sbatch
+sbatch 005_noise-unit10_1_unit10.sbatch
+
+sbatch 005_noise-unit10_2_unit01.sbatch
+sbatch 005_noise-unit10_2_unit02.sbatch
+sbatch 005_noise-unit10_2_unit03.sbatch
+sbatch 005_noise-unit10_2_unit05.sbatch
+sbatch 005_noise-unit10_2_unit07.sbatch
+sbatch 005_noise-unit10_2_unit10.sbatch
+
+sbatch 005_noise-unit10_3_unit01.sbatch
+sbatch 005_noise-unit10_3_unit02.sbatch
+sbatch 005_noise-unit10_3_unit03.sbatch
+sbatch 005_noise-unit10_3_unit05.sbatch
+sbatch 005_noise-unit10_3_unit07.sbatch
+sbatch 005_noise-unit10_3_unit10.sbatch
+
+
+sbatch 005_stretch_0_unit01.sbatch
+sbatch 005_stretch_0_unit02.sbatch
+sbatch 005_stretch_0_unit03.sbatch
+sbatch 005_stretch_0_unit05.sbatch
+sbatch 005_stretch_0_unit07.sbatch
+sbatch 005_stretch_0_unit10.sbatch
+
+sbatch 005_stretch_1_unit01.sbatch
+sbatch 005_stretch_1_unit02.sbatch
+sbatch 005_stretch_1_unit03.sbatch
+sbatch 005_stretch_1_unit05.sbatch
+sbatch 005_stretch_1_unit07.sbatch
+sbatch 005_stretch_1_unit10.sbatch
+
+sbatch 005_stretch_2_unit01.sbatch
+sbatch 005_stretch_2_unit02.sbatch
+sbatch 005_stretch_2_unit03.sbatch
+sbatch 005_stretch_2_unit05.sbatch
+sbatch 005_stretch_2_unit07.sbatch
+sbatch 005_stretch_2_unit10.sbatch
+
+sbatch 005_stretch_3_unit01.sbatch
+sbatch 005_stretch_3_unit02.sbatch
+sbatch 005_stretch_3_unit03.sbatch
+sbatch 005_stretch_3_unit05.sbatch
+sbatch 005_stretch_3_unit07.sbatch
+sbatch 005_stretch_3_unit10.sbatch
+
+
+sbatch 005_noise-unit02_0_unit01.sbatch
+sbatch 005_noise-unit02_0_unit02.sbatch
+sbatch 005_noise-unit02_0_unit03.sbatch
+sbatch 005_noise-unit02_0_unit05.sbatch
+sbatch 005_noise-unit02_0_unit07.sbatch
+sbatch 005_noise-unit02_0_unit10.sbatch
+
+sbatch 005_noise-unit02_1_unit01.sbatch
+sbatch 005_noise-unit02_1_unit02.sbatch
+sbatch 005_noise-unit02_1_unit03.sbatch
+sbatch 005_noise-unit02_1_unit05.sbatch
+sbatch 005_noise-unit02_1_unit07.sbatch
+sbatch 005_noise-unit02_1_unit10.sbatch
+
+sbatch 005_noise-unit02_2_unit01.sbatch
+sbatch 005_noise-unit02_2_unit02.sbatch
+sbatch 005_noise-unit02_2_unit03.sbatch
+sbatch 005_noise-unit02_2_unit05.sbatch
+sbatch 005_noise-unit02_2_unit07.sbatch
+sbatch 005_noise-unit02_2_unit10.sbatch
+
+sbatch 005_noise-unit02_3_unit01.sbatch
+sbatch 005_noise-unit02_3_unit02.sbatch
+sbatch 005_noise-unit02_3_unit03.sbatch
+sbatch 005_noise-unit02_3_unit05.sbatch
+sbatch 005_noise-unit02_3_unit07.sbatch
+sbatch 005_noise-unit02_3_unit10.sbatch
+
+
+sbatch 005_original_unit01.sbatch
+sbatch 005_original_unit02.sbatch
+sbatch 005_original_unit03.sbatch
+sbatch 005_original_unit05.sbatch
+sbatch 005_original_unit07.sbatch
+sbatch 005_original_unit10.sbatch
+
+
+sbatch 005_noise-unit01_0_unit01.sbatch
+sbatch 005_noise-unit01_0_unit02.sbatch
+sbatch 005_noise-unit01_0_unit03.sbatch
+sbatch 005_noise-unit01_0_unit05.sbatch
+sbatch 005_noise-unit01_0_unit07.sbatch
+sbatch 005_noise-unit01_0_unit10.sbatch
+
+sbatch 005_noise-unit01_1_unit01.sbatch
+sbatch 005_noise-unit01_1_unit02.sbatch
+sbatch 005_noise-unit01_1_unit03.sbatch
+sbatch 005_noise-unit01_1_unit05.sbatch
+sbatch 005_noise-unit01_1_unit07.sbatch
+sbatch 005_noise-unit01_1_unit10.sbatch
+
+sbatch 005_noise-unit01_2_unit01.sbatch
+sbatch 005_noise-unit01_2_unit02.sbatch
+sbatch 005_noise-unit01_2_unit03.sbatch
+sbatch 005_noise-unit01_2_unit05.sbatch
+sbatch 005_noise-unit01_2_unit07.sbatch
+sbatch 005_noise-unit01_2_unit10.sbatch
+
+sbatch 005_noise-unit01_3_unit01.sbatch
+sbatch 005_noise-unit01_3_unit02.sbatch
+sbatch 005_noise-unit01_3_unit03.sbatch
+sbatch 005_noise-unit01_3_unit05.sbatch
+sbatch 005_noise-unit01_3_unit07.sbatch
+sbatch 005_noise-unit01_3_unit10.sbatch
+
+
+sbatch 005_noise-unit07_0_unit01.sbatch
+sbatch 005_noise-unit07_0_unit02.sbatch
+sbatch 005_noise-unit07_0_unit03.sbatch
+sbatch 005_noise-unit07_0_unit05.sbatch
+sbatch 005_noise-unit07_0_unit07.sbatch
+sbatch 005_noise-unit07_0_unit10.sbatch
+
+sbatch 005_noise-unit07_1_unit01.sbatch
+sbatch 005_noise-unit07_1_unit02.sbatch
+sbatch 005_noise-unit07_1_unit03.sbatch
+sbatch 005_noise-unit07_1_unit05.sbatch
+sbatch 005_noise-unit07_1_unit07.sbatch
+sbatch 005_noise-unit07_1_unit10.sbatch
+
+sbatch 005_noise-unit07_2_unit01.sbatch
+sbatch 005_noise-unit07_2_unit02.sbatch
+sbatch 005_noise-unit07_2_unit03.sbatch
+sbatch 005_noise-unit07_2_unit05.sbatch
+sbatch 005_noise-unit07_2_unit07.sbatch
+sbatch 005_noise-unit07_2_unit10.sbatch
+
+sbatch 005_noise-unit07_3_unit01.sbatch
+sbatch 005_noise-unit07_3_unit02.sbatch
+sbatch 005_noise-unit07_3_unit03.sbatch
+sbatch 005_noise-unit07_3_unit05.sbatch
+sbatch 005_noise-unit07_3_unit07.sbatch
+sbatch 005_noise-unit07_3_unit10.sbatch
+
+
+sbatch 005_noise-unit03_0_unit01.sbatch
+sbatch 005_noise-unit03_0_unit02.sbatch
+sbatch 005_noise-unit03_0_unit03.sbatch
+sbatch 005_noise-unit03_0_unit05.sbatch
+sbatch 005_noise-unit03_0_unit07.sbatch
+sbatch 005_noise-unit03_0_unit10.sbatch
+
+sbatch 005_noise-unit03_1_unit01.sbatch
+sbatch 005_noise-unit03_1_unit02.sbatch
+sbatch 005_noise-unit03_1_unit03.sbatch
+sbatch 005_noise-unit03_1_unit05.sbatch
+sbatch 005_noise-unit03_1_unit07.sbatch
+sbatch 005_noise-unit03_1_unit10.sbatch
+
+sbatch 005_noise-unit03_2_unit01.sbatch
+sbatch 005_noise-unit03_2_unit02.sbatch
+sbatch 005_noise-unit03_2_unit03.sbatch
+sbatch 005_noise-unit03_2_unit05.sbatch
+sbatch 005_noise-unit03_2_unit07.sbatch
+sbatch 005_noise-unit03_2_unit10.sbatch
+
+sbatch 005_noise-unit03_3_unit01.sbatch
+sbatch 005_noise-unit03_3_unit02.sbatch
+sbatch 005_noise-unit03_3_unit03.sbatch
+sbatch 005_noise-unit03_3_unit05.sbatch
+sbatch 005_noise-unit03_3_unit07.sbatch
+sbatch 005_noise-unit03_3_unit10.sbatch
+
+
+sbatch 005_pitch_0_unit01.sbatch
+sbatch 005_pitch_0_unit02.sbatch
+sbatch 005_pitch_0_unit03.sbatch
+sbatch 005_pitch_0_unit05.sbatch
+sbatch 005_pitch_0_unit07.sbatch
+sbatch 005_pitch_0_unit10.sbatch
+
+sbatch 005_pitch_1_unit01.sbatch
+sbatch 005_pitch_1_unit02.sbatch
+sbatch 005_pitch_1_unit03.sbatch
+sbatch 005_pitch_1_unit05.sbatch
+sbatch 005_pitch_1_unit07.sbatch
+sbatch 005_pitch_1_unit10.sbatch
+
+sbatch 005_pitch_2_unit01.sbatch
+sbatch 005_pitch_2_unit02.sbatch
+sbatch 005_pitch_2_unit03.sbatch
+sbatch 005_pitch_2_unit05.sbatch
+sbatch 005_pitch_2_unit07.sbatch
+sbatch 005_pitch_2_unit10.sbatch
+
+sbatch 005_pitch_3_unit01.sbatch
+sbatch 005_pitch_3_unit02.sbatch
+sbatch 005_pitch_3_unit03.sbatch
+sbatch 005_pitch_3_unit05.sbatch
+sbatch 005_pitch_3_unit07.sbatch
+sbatch 005_pitch_3_unit10.sbatch
+
+
