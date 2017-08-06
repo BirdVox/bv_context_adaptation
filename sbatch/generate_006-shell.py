3a7cc339a1ab1f185bb65a8dee235c92e25f1bac
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 19:36:47 -0400

    write generate_006-shell

---
+++ sbatch/generate_006-shell.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.append("../src")
+import localmodule
+
+# Define constants.
+units = localmodule.get_units()
+file_path = "006.sh"
+
+
+# Open shell file.
+with open(file_path, "w") as f:
+    # Print header.
+    f.write(
+        "# This shell script executes the Slurm jobs for computing " +
+        "log-mel-spectrograms on continuous audio.\n")
+    f.write("\n")
+
+    # Loop over recording units.
+    for unit_str in units:
+        # Define job name.
+        job_name = "_".join(["005", unit_str])
+        sbatch_str = "sbatch " + job_name + ".sbatch"
+
+        # Write SBATCH command to shell file.
+        f.write(sbatch_str + "\n")
+    f.write("\n")
+
+
+# Grant permission to execute the shell file.
+# https://stackoverflow.com/a/30463972
+mode = os.stat(file_path).st_mode
+mode |= (mode & 0o444) >> 2
+os.chmod(file_path, mode)
