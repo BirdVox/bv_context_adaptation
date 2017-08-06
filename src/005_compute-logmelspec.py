14e077dd4b148e6f34dc52e642346a777cdd45be
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-06 14:21:35 -0400

    quater

--- src/005_compute-logmelspec.py
+++ src/005_compute-logmelspec.py
@@ -12,8 +12,7 @@ import localmodule
 data_dir = localmodule.get_data_dir()
 dataset_name = localmodule.get_dataset_name()
 sample_rate = localmodule.get_sample_rate()
-args = ["stretch", 0, "unit01"] #                          DISABLE ME
-#args = sys.argv[1:]                                         ENABLE ME
+args = sys.argv[1:]
 aug_str = args[0]
 instance_id = int(args[1])
 instance_str = str(instance_id)
@@ -44,6 +43,7 @@ in_aug_dir = os.path.join(hdf5_dir, aug_str)
 hdf5_name = "_".join([dataset_name, instanced_aug_str, unit_str])
 in_path = os.path.join(in_aug_dir, hdf5_name + ".hdf5")
 in_file = h5py.File(in_path, "r")
+sample_rate = in_file["sample_rate"].value
 
 
 # Create HDF5 container of logmelspecs
@@ -63,19 +63,63 @@ out_file["augmentation"] = aug_str
 out_file["instance"] = instance_id
 out_file["utc_start_time"] = in_file["utc_start_time"].value
 gps_group = out_file.create_group("gps_coordinates")
-gps_group["latitude"] = in_file["gps_coordinates"]["latitude"]
-gps_group["longitude"] = in_file["gps_coordinates"]
-lms_group = out_file.create_group("logmelspec_settings")
-lms_group["fmax"] = logmelspec_settings["fmax"]
-lms_group["fmin"] = logmelspec_settings["fmin"]
-lms_group["hop_length"] = logmelspec_settings["hop_length"]
-lms_group["n_fft"] = logmelspec_settings["n_fft"]
-lms_group["n_mels"] = logmelspec_settings["n_mels"]
-lms_group["sr"] = logmelspec_settings["sr"]
-lms_group["win_length"] = logmelspec_settings["win_length"]
-lms_group["window"] = logmelspec_settings["window"]
-#waveforms = in_file["waveforms"]
-#durations = [waveforms[key].shape[0] for key in waveforms.keys()]
+gps_group["latitude"] = in_file["gps_coordinates"]["latitude"].value
+gps_group["longitude"] = in_file["gps_coordinates"]["longitude"].value
+settings_group = out_file.create_group("logmelspec_settings")
+settings_group["fmax"] = logmelspec_settings["fmax"]
+settings_group["fmin"] = logmelspec_settings["fmin"]
+settings_group["hop_length"] = logmelspec_settings["hop_length"]
+settings_group["n_fft"] = logmelspec_settings["n_fft"]
+settings_group["n_mels"] = logmelspec_settings["n_mels"]
+settings_group["sr"] = logmelspec_settings["sr"]
+settings_group["win_length"] = logmelspec_settings["win_length"]
+settings_group["window"] = logmelspec_settings["window"]
+
+
+# List clips
+lms_group = out_file.create_group("logmelspec")
+clip_names = list(in_file["waveforms"].keys())
+
+
+# Loop over clips
+for clip_name in clip_names:
+    # Load waveform
+    waveform = in_file["waveforms"][clip_name].value
+
+    # Resample to 22050 Hz
+    waveform = librosa.resample(
+        waveform, sample_rate, logmelspec_settings["sr"])
+
+    # Compute Short-Term Fourier Transform (STFT)
+    stft = librosa.stft(
+        waveform,
+        n_fft=logmelspec_settings["n_fft"],
+        win_length=logmelspec_settings["win_length"],
+        hop_length=logmelspec_settings["hop_length"],
+        window=logmelspec_settings["window"])
+
+    # Compute squared magnitude coefficients
+    abs2_stft = (stft.real*stft.real) + (stft.imag*stft.imag)
+
+    # Gather frequency bins according to the Mel scale
+    melspec = librosa.feature.melspectrogram(
+        y=None,
+        S=abs2_stft,
+        sr=logmelspec_settings["sr"],
+        n_fft=logmelspec_settings["n_fft"],
+        n_mels=logmelspec_settings["n_mels"],
+        htk=True,
+        fmin=logmelspec_settings["fmin"],
+        fmax=logmelspec_settings["fmax"])
+
+    # Apply pointwise base-10 logarithm
+    logmelspec = 0.5 * librosa.logamplitude(melspec, ref=1.0)
+
+    # Convert to single floating-point precision
+    logmelspec = logmelspec.astype('float32')
+
+    # Save
+    lms_group[clip_name] = logmelspec
 
 
 # Print elapsed time.
