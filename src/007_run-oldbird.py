156b275c8cd8282bcbe6b94d96d7a074786d1c60
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-07 13:49:26 -0400

    finish writing 007 (debug mode)

--- src/007_run-oldbird.py
+++ src/007_run-oldbird.py
@@ -1,4 +1,5 @@
 import datetime
+import h5py
 import itertools
 import math
 import numpy as np
@@ -48,6 +49,38 @@ print('soundfile version: {:s}'.format(sf.__version__))
 print("")
 
 
+# Create HDF5 container of ODF curves
+full_oldbird_name = "_".join([dataset_name, "oldbird"])
+full_oldbird_dir = os.path.join(data_dir, full_oldbird_name)
+os.makedirs(full_oldbird_dir, exist_ok=True)
+out_name = unit_str
+out_path = os.path.join(full_oldbird_dir, out_name + ".hdf5")
+out_file = h5py.File(out_path)
+
+
+# Load GPS coordinates.
+gps_name = "_".join([dataset_name, "gps-coordinates.csv"])
+gps_path = os.path.join(data_dir, gps_name)
+gps_df = pd.read_csv(gps_path)
+gps_row = gps_df.loc[gps_df["Unit"] == unit_str].iloc[0]
+
+
+# Load UTC starting times.
+utc_name = "_".join([dataset_name, "utc-start-times.csv"])
+utc_path = os.path.join(data_dir, utc_name)
+utc_df = pd.read_csv(utc_path)
+utc_row = utc_df.loc[utc_df["Unit"] == unit_str].iloc[0]
+
+
+# Copy over metadata.
+out_file["dataset_name"] = dataset_name
+out_file["unit"] = unit_str
+out_file["utc_start_time"] = utc_row["UTC"]
+gps_group = out_file.create_group("gps_coordinates")
+gps_group["latitude"] =  gps_row["Latitude"]
+gps_group["longitude"] = gps_row["Longitude"]
+
+
 # Design Thrush filter and integrator.
 thrush_settings = ob._THRUSH_SETTINGS
 thrush_fir = design_oldbird_filter(thrush_settings)
@@ -56,6 +89,9 @@ thrush_integration_length = int(round(thrush_integration_time * sample_rate))
 thrush_integrator = np.ones(thrush_integration_length)
 thrush_integrator = thrush_integrator / thrush_integration_length
 thrush_delay = math.floor(thrush_settings.ratio_delay * sample_rate)
+thrush_group = out_file.create_group("thrush_settings")
+for thrush_key in thrush_settings:
+    thrush_group[thrush_key] = thrush_settings[thrush_key]
 
 
 # Design Tseep filter and integrator.
@@ -66,6 +102,8 @@ tseep_integration_length = int(round(tseep_integration_time * sample_rate))
 tseep_integrator = np.ones(tseep_integration_length)
 tseep_integrator = tseep_integrator / tseep_integration_length
 tseep_delay = math.floor(tseep_settings.ratio_delay * sample_rate)
+for tseep_key in tseep_settings:
+    tseep_group[tseep_key] = tseep_settings[tseep_key]
 
 
 # Count number of chunks.
@@ -75,13 +113,15 @@ full_audio_length = len(full_audio_object)
 n_chunks = int(np.ceil(full_audio_length / chunk_length))
 
 
-# Initialize lists of onset detection functions (ODF).
-thrush_chunk_odfs = []
-tseep_chunk_odfs = []
+# Initialize datasets of onset detection functions (ODF).
+dataset_size = (1, full_audio_length)
+thrush_dataset = out_file.create_dataset("thrush_odf", dataset_size)
+tseep_dataset = out_file.create_dataset("tseep_odf", dataset_size)
 
 
 # Loop over chunks.
-for chunk_id in range(n_chunks):
+#for chunk_id in range(n_chunks):
+for chunk_id in [0, n_chunks-1]:
     # Load audio excerpt.
     chunk_start = chunk_id * chunk_length
     chunk_stop = min(chunk_start + chunk_length, full_audio_length)
@@ -115,7 +155,7 @@ for chunk_id in range(n_chunks):
     thrush_odf = delayed_thrush / integrated_thrush
     thrush_chunk_odf = thrush_odf[chunk_padding_length:-chunk_padding_length]
     thrush_chunk_odf = thrush_chunk_odf.astype('float32')
-    thrush_chunk_odfs.append(thrush_chunk_odf)
+    thrush_dataset[0, chunk_start:chunk_stop] = thrush_chunk_odf
 
     # Apply Tseep filter, square, integrate, divide by delayed signal.
     fir_tseep = scipy.signal.fftconvolve(padded_chunk, tseep_fir, mode="same")
@@ -129,46 +169,7 @@ for chunk_id in range(n_chunks):
     tseep_odf = delayed_tseep / integrated_tseep
     tseep_chunk_odf = tseep_odf[chunk_padding_length:-chunk_padding_length]
     tseep_chunk_odf = tseep_chunk_odf.astype('float32')
-    tseep_chunk_odfs.append(tseep_chunk_odf)
-
-
-# Concatenate chunk-wise ODFs to get full ODFs for the whole unit.
-thrush_odf = np.concatenate(thrush_chunk_odfs)
-tseep_odf = np.concatenate(tseep_chunk_odfs)
-
-
-# Renormalize Thrush and Tseep ODFs by their respective thresholds, and sum
-# the results to get a global ODF. This works because both Thrush and Tseep
-# yield nonnegative ODFs.
-thrush_threshold = thrush_settings.ratio_threshold
-tseep_threshold = tseep_settings.ratio_threshold
-global_odf = 0.5 * (thrush_odf/thrush_threshold + tseep_odf/tseep_threshold)
-
-
-# Build NumPy matrices. The first column is a timestamp, the second column is
-# the unnormalized probability.
-time = np.arange(0, full_audio_length)
-thrush_matrix = np.stack((time, thrush_odf), axis=-1)
-tseep_matrix = np.stack((time, tseep_odf), axis=-1)
-global_matrix = np.stack((time, global_odf), axis=-1)
-
-
-# Export matrices related to Thrush, Tseep, and "global".
-models_dir = localmodule.get_data_dir()
-model_name = os.path.basename(__file__).split("_")[0]
-model_dir = os.path.join(models_dir, model_name)
-os.makedirs(model_dir, exist_ok=True)
-out_unit_dir = os.path.join(model_dir, unit_str)
-os.makedirs(out_unit_dir, exist_ok=True)
-thrush_filename = "_".join([model_name, "thrush-odf", unit_str, "0"]) + ".npy"
-thrush_path = os.path.join(out_unit_dir, thrush_filename)
-np.save(thrush_path, thrush_matrix)
-tseep_filename = "_".join([model_name, "tseep-odf", unit_str, "0"]) + ".npy"
-tseep_path = os.path.join(out_unit_dir, tseep_filename)
-np.save(tseep_path, tseep_matrix)
-global_filename = "_".join([model_name, "odf", unit_str, "0"]) + ".npy"
-global_path = os.path.join(out_unit_dir, global_filename)
-np.save(global_path, global_matrix)
+    tseep_dataset[0, chunk_start:chunk_stop] = tseep_chunk_odf
 
 
 # Print elapsed time.
