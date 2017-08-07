af0f1272e4503568978853dbcc9023355c4b857a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-07 17:45:50 -0400

    generate 008.sh

---
+++ sbatch/008.sh
@@ -0,0 +1,140 @@
+# This shell script executes all Slurm jobs for thresholding Old Bird onset detection functions: Thrush and Tseep.
+
+sbatch 008_unit01_thrush_00:09.sbatch
+sbatch 008_unit01_thrush_10:19.sbatch
+sbatch 008_unit01_thrush_20:29.sbatch
+sbatch 008_unit01_thrush_30:39.sbatch
+sbatch 008_unit01_thrush_40:49.sbatch
+sbatch 008_unit01_thrush_50:59.sbatch
+sbatch 008_unit01_thrush_60:69.sbatch
+sbatch 008_unit01_thrush_70:79.sbatch
+sbatch 008_unit01_thrush_80:89.sbatch
+sbatch 008_unit01_thrush_90:99.sbatch
+
+sbatch 008_unit01_tseep_00:09.sbatch
+sbatch 008_unit01_tseep_10:19.sbatch
+sbatch 008_unit01_tseep_20:29.sbatch
+sbatch 008_unit01_tseep_30:39.sbatch
+sbatch 008_unit01_tseep_40:49.sbatch
+sbatch 008_unit01_tseep_50:59.sbatch
+sbatch 008_unit01_tseep_60:69.sbatch
+sbatch 008_unit01_tseep_70:79.sbatch
+sbatch 008_unit01_tseep_80:89.sbatch
+sbatch 008_unit01_tseep_90:99.sbatch
+
+
+sbatch 008_unit02_thrush_00:09.sbatch
+sbatch 008_unit02_thrush_10:19.sbatch
+sbatch 008_unit02_thrush_20:29.sbatch
+sbatch 008_unit02_thrush_30:39.sbatch
+sbatch 008_unit02_thrush_40:49.sbatch
+sbatch 008_unit02_thrush_50:59.sbatch
+sbatch 008_unit02_thrush_60:69.sbatch
+sbatch 008_unit02_thrush_70:79.sbatch
+sbatch 008_unit02_thrush_80:89.sbatch
+sbatch 008_unit02_thrush_90:99.sbatch
+
+sbatch 008_unit02_tseep_00:09.sbatch
+sbatch 008_unit02_tseep_10:19.sbatch
+sbatch 008_unit02_tseep_20:29.sbatch
+sbatch 008_unit02_tseep_30:39.sbatch
+sbatch 008_unit02_tseep_40:49.sbatch
+sbatch 008_unit02_tseep_50:59.sbatch
+sbatch 008_unit02_tseep_60:69.sbatch
+sbatch 008_unit02_tseep_70:79.sbatch
+sbatch 008_unit02_tseep_80:89.sbatch
+sbatch 008_unit02_tseep_90:99.sbatch
+
+
+sbatch 008_unit03_thrush_00:09.sbatch
+sbatch 008_unit03_thrush_10:19.sbatch
+sbatch 008_unit03_thrush_20:29.sbatch
+sbatch 008_unit03_thrush_30:39.sbatch
+sbatch 008_unit03_thrush_40:49.sbatch
+sbatch 008_unit03_thrush_50:59.sbatch
+sbatch 008_unit03_thrush_60:69.sbatch
+sbatch 008_unit03_thrush_70:79.sbatch
+sbatch 008_unit03_thrush_80:89.sbatch
+sbatch 008_unit03_thrush_90:99.sbatch
+
+sbatch 008_unit03_tseep_00:09.sbatch
+sbatch 008_unit03_tseep_10:19.sbatch
+sbatch 008_unit03_tseep_20:29.sbatch
+sbatch 008_unit03_tseep_30:39.sbatch
+sbatch 008_unit03_tseep_40:49.sbatch
+sbatch 008_unit03_tseep_50:59.sbatch
+sbatch 008_unit03_tseep_60:69.sbatch
+sbatch 008_unit03_tseep_70:79.sbatch
+sbatch 008_unit03_tseep_80:89.sbatch
+sbatch 008_unit03_tseep_90:99.sbatch
+
+
+sbatch 008_unit05_thrush_00:09.sbatch
+sbatch 008_unit05_thrush_10:19.sbatch
+sbatch 008_unit05_thrush_20:29.sbatch
+sbatch 008_unit05_thrush_30:39.sbatch
+sbatch 008_unit05_thrush_40:49.sbatch
+sbatch 008_unit05_thrush_50:59.sbatch
+sbatch 008_unit05_thrush_60:69.sbatch
+sbatch 008_unit05_thrush_70:79.sbatch
+sbatch 008_unit05_thrush_80:89.sbatch
+sbatch 008_unit05_thrush_90:99.sbatch
+
+sbatch 008_unit05_tseep_00:09.sbatch
+sbatch 008_unit05_tseep_10:19.sbatch
+sbatch 008_unit05_tseep_20:29.sbatch
+sbatch 008_unit05_tseep_30:39.sbatch
+sbatch 008_unit05_tseep_40:49.sbatch
+sbatch 008_unit05_tseep_50:59.sbatch
+sbatch 008_unit05_tseep_60:69.sbatch
+sbatch 008_unit05_tseep_70:79.sbatch
+sbatch 008_unit05_tseep_80:89.sbatch
+sbatch 008_unit05_tseep_90:99.sbatch
+
+
+sbatch 008_unit07_thrush_00:09.sbatch
+sbatch 008_unit07_thrush_10:19.sbatch
+sbatch 008_unit07_thrush_20:29.sbatch
+sbatch 008_unit07_thrush_30:39.sbatch
+sbatch 008_unit07_thrush_40:49.sbatch
+sbatch 008_unit07_thrush_50:59.sbatch
+sbatch 008_unit07_thrush_60:69.sbatch
+sbatch 008_unit07_thrush_70:79.sbatch
+sbatch 008_unit07_thrush_80:89.sbatch
+sbatch 008_unit07_thrush_90:99.sbatch
+
+sbatch 008_unit07_tseep_00:09.sbatch
+sbatch 008_unit07_tseep_10:19.sbatch
+sbatch 008_unit07_tseep_20:29.sbatch
+sbatch 008_unit07_tseep_30:39.sbatch
+sbatch 008_unit07_tseep_40:49.sbatch
+sbatch 008_unit07_tseep_50:59.sbatch
+sbatch 008_unit07_tseep_60:69.sbatch
+sbatch 008_unit07_tseep_70:79.sbatch
+sbatch 008_unit07_tseep_80:89.sbatch
+sbatch 008_unit07_tseep_90:99.sbatch
+
+
+sbatch 008_unit10_thrush_00:09.sbatch
+sbatch 008_unit10_thrush_10:19.sbatch
+sbatch 008_unit10_thrush_20:29.sbatch
+sbatch 008_unit10_thrush_30:39.sbatch
+sbatch 008_unit10_thrush_40:49.sbatch
+sbatch 008_unit10_thrush_50:59.sbatch
+sbatch 008_unit10_thrush_60:69.sbatch
+sbatch 008_unit10_thrush_70:79.sbatch
+sbatch 008_unit10_thrush_80:89.sbatch
+sbatch 008_unit10_thrush_90:99.sbatch
+
+sbatch 008_unit10_tseep_00:09.sbatch
+sbatch 008_unit10_tseep_10:19.sbatch
+sbatch 008_unit10_tseep_20:29.sbatch
+sbatch 008_unit10_tseep_30:39.sbatch
+sbatch 008_unit10_tseep_40:49.sbatch
+sbatch 008_unit10_tseep_50:59.sbatch
+sbatch 008_unit10_tseep_60:69.sbatch
+sbatch 008_unit10_tseep_70:79.sbatch
+sbatch 008_unit10_tseep_80:89.sbatch
+sbatch 008_unit10_tseep_90:99.sbatch
+
+
