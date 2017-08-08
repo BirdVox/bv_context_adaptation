0db9bb4a34e029d20853e37e83366dc42f13b646
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-08 16:46:17 -0400

    update 008

--- src/008_threshold-oldbird.py
+++ src/008_threshold-oldbird.py
@@ -2,12 +2,13 @@ import datetime
 import h5py
 import numpy as np
 import os
+import sys
 import time
 
 import localmodule
 
 
-# Define constants
+# Define constants.
 n_thresholds = 100
 # The array of upward thresholds is equal to
 # f(t) = 1 + threshold_multiplier * (t**threshold_exponent)
@@ -21,8 +22,9 @@ data_dir = localmodule.get_data_dir()
 dataset_name = localmodule.get_dataset_name()
 models_dir = localmodule.get_models_dir()
 sample_rate = localmodule.get_sample_rate()
-args = ["unit01", "thrush", "00:09"] #                          DISABLE
-#args = sys.argv[1:]                                            ENABLE
+hop_duration = 0.02 # in seconds
+hop_length = int(np.round(sample_rate * hop_duration))
+args = sys.argv[1:]
 unit_str = args[0]
 odf_str = args[1]
 threshold_id_start = int(args[2][:2])
@@ -43,7 +45,7 @@ print("")
 # Load onset detection function.
 oldbird_name = "_".join([dataset_name, "oldbird"])
 oldbird_dir = os.path.join(data_dir, oldbird_name)
-odf_path = os.path.join(data_dir, unit_str + ".hdf5")
+odf_path = os.path.join(oldbird_dir, unit_str + ".hdf5")
 odf_file = h5py.File(odf_path, "r")
 odf_dataset_key = "_".join([odf_str, "odf"])
 odf = odf_file[odf_dataset_key]
@@ -53,14 +55,16 @@ odf_length = len(odf)
 # Define arrays of thresholds.
 odf_settings_key = "_".join([odf_str, "settings"])
 odf_settings = odf_file[odf_settings_key]
-ad_hoc_threshold = odf_settings["ratio_threshold"]
-threshold_multiplier = np.log(ad_hoc_threshold**2 - 1)
-threshold_exponent =
-    (np.log(ad_hoc_threshold-1) * threshold_multiplier) /
-    np.log(threshold_alpha_range)
-up_threshold_abcissa = np.arange(1, 1+n_thresholds)
-up_thresholds = threshold_multiplier * up_threshold_abcissa**threshold_exponent
-down_thresholds = np.tensorinv(up_thresholds)
+ad_hoc_threshold = odf_settings["ratio_threshold"].value
+ad_hoc_threshold = 1.2
+threshold_multiplier = ad_hoc_threshold**2 - 1
+threshold_exponent = \
+    (np.log(ad_hoc_threshold-1) - np.log(threshold_multiplier)) /\
+    np.log(threshold_range_alpha)
+up_threshold_abcissa = np.arange(1, 1+n_thresholds) / n_thresholds
+up_thresholds = 1 +\
+    threshold_multiplier * up_threshold_abcissa**threshold_exponent
+down_thresholds = 1.0 / up_thresholds
 
 
 # Create directory for Old Bird in models_dir.
@@ -72,16 +76,12 @@ predictions_dir = os.path.join(out_unit_dir, "predictions")
 os.makedirs(predictions_dir, exist_ok=True)
 
 
-# Loop over thresholds in group.
-for threshold_id in threshold_id_range:
-    up_threshold = up_thresholds[threshold_id]
-    down_threshold = down_thresholds[threshold_id]
+# Compute minimum and maximum clip length according to Old Bird heuristics.
+min_clip_duration = odf_settings["min_duration"].value
+min_clip_length = int(np.round(min_clip_duration * sample_rate))
+max_clip_duration = odf_settings["max_duration"].value
+max_clip_length = int(np.round(max_clip_duration * sample_rate))
 
-    # Define CSV file for given threshold.
-    # TODO.
-
-    # Loop over timestamps.
-    # TODO
 
 
 # Print elapsed time.
