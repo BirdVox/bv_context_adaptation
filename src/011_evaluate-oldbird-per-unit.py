173b610297e3914f9a84c4327a216b98d44579af
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-09 05:33:27 -0400

    include "merged" odf in 011

--- src/011_evaluate-oldbird-per-unit.py
+++ src/011_evaluate-oldbird-per-unit.py
@@ -18,8 +18,7 @@ models_dir = localmodule.get_models_dir()
 negative_labels = localmodule.get_negative_labels()
 tolerances = localmodule.get_tolerances()
 units = localmodule.get_units()
-#odfs = ["thrush", "tseep", "merged"]
-odfs = ["thrush", "tseep"]
+odfs = ["thrush", "tseep", "merged"]
 clip_suppressor_modes = ["no-clip-suppressor", "clip-suppressor"]
 n_thresholds = 100
 
