385a3fca7ffdfe637838fbd469f44ba68f610308
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-08 22:39:06 -0400

    start 009

---
+++ src/009_suppress-clips-oldbird.py
@@ -0,0 +1,29 @@
+import datetime
+import numpy as np
+import os
+import pandas as pd
+import sys
+import time
+
+import localmodule
+
+
+# Print header.
+start_time = int(time.time())
+print(str(datetime.datetime.now()) + " Start.")
+print("Running Old Bird onset detection functions (Thrush and Tseep) on " +
+    dataset_name + ", " + unit_str + ".")
+print('pandas version: {:s}'.format(pd.__version__))
+print("")
+
+
+# Print elapsed time.
+print(str(datetime.datetime.now()) + " Finish.")
+elapsed_time = time.time() - int(start_time)
+elapsed_hours = int(elapsed_time / (60 * 60))
+elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
+elapsed_seconds = elapsed_time % 60.
+elapsed_str = "{:>02}:{:>02}:{:>05.2f}".format(elapsed_hours,
+                                               elapsed_minutes,
+                                               elapsed_seconds)
+print("Total elapsed time: " + elapsed_str + ".")
