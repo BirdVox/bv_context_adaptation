8dfce29ccd126a384459b064a54620f31e839a11
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-13 18:25:34 -0400

    update 014

--- src/014_predict-icassp-convnet-clips.py
+++ src/014_predict-icassp-convnet-clips.py
@@ -16,6 +16,7 @@ import localmodule
 dataset_name = localmodule.get_dataset_name()
 folds = localmodule.fold_units()
 models_dir = localmodule.get_models_dir()
+n_input_hops = 104
 
 
 # Read command-line arguments.
@@ -65,12 +66,47 @@ network_path = os.path.join(trial_dir, network_name + ".hdf5")
 model = keras.models.load_model(network_path)
 
 
-# Make Pescador streamer for prection unit.
+# Open logmelspec container with h5py.
 aug_str = "original"
 hdf5_dir = os.path.join(data_dir, "_".join([dataset_name, "hdf5"]))
 original_hdf5_dir = os.path.join(hdf5_dir, aug_str)
 hdf5_name = "_".join([dataset_name, aug_str, predict_unit_str])
 hdf5_path = os.path.join(original_hdf5_dir, hdf5_name + ".hdf5")
+lms_container = h5py.File(hdf5_path, "r")
+lms_group = lms_container["logmelspec"]
+
+# Create HDF5 container for predictions.
+clip_predictions_name = "_".join([
+    dataset_name,
+    model_name,
+    "test-" + test_unit_str,
+    "predict-" + predict_unit_str,
+    "clip-predictions"
+])
+
+keys = list(lms_group.keys())
+for key in keys:
+    # Load logmelspec.
+    X = lms_group[key]
+
+    # Trim logmelspec in time to required number of hops.
+    X_width = X.shape[1]
+    first_col = int((X_width-n_input_hops) / 2)
+    last_col = int((X_width+n_input_hops) / 2)
+    X = X[:, first_col:last_col]
+
+    # Add trailing singleton dimension for Keras interoperability.
+    X = X[:, :, np.newaxis]
+
+    # Predict.
+    y = model.predict(X)
+
+    # Store prediction in dictionary.
+    prediction[key] = y
+
+
+# Close HDF5 containers.
+hdf5_file.close()
 
 
 # Print elapsed time.
