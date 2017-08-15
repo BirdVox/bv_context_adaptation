1d562db46cce0e5f07208e177f434b345ba44eaf
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-15 18:27:00 -0400

    bugfix yield_logmelspec

--- src/localmodule.py
+++ src/localmodule.py
@@ -226,7 +226,7 @@ def yield_logmelspec(lms_path, n_hops):
             X = X[:, :, np.newaxis]
 
             # Retrieve label y from key name.
-            y = np.float32(key.split("_")[3])
+            y = np.array([np.float32(key.split("_")[3])])
 
             # Yield data and label as dictionary.
             yield dict(X=X, y=y)
