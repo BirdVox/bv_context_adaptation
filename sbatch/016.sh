caa1fbb65df0057837dee0e2781ee79c189c8633
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-20 17:35:18 -0400

    generate 016.sh

---
+++ sbatch/016.sh
@@ -0,0 +1,9 @@
+# This shell script executes the Slurm jobs for computing spectral flux on continuous audio.
+
+sbatch 016_unit01.sbatch
+sbatch 016_unit02.sbatch
+sbatch 016_unit03.sbatch
+sbatch 016_unit05.sbatch
+sbatch 016_unit07.sbatch
+sbatch 016_unit10.sbatch
+
