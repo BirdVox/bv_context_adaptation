338916ff44fcb69688e21f14429387242967cfa6
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-20 19:33:47 -0400

    start 017_threshold-spectral-flux

---
+++ src/017_threshold-spectral-flux.py
@@ -0,0 +1,40 @@
+import csv
+import datetime
+import h5py
+import numpy as np
+import os
+import sys
+import time
+
+import localmodule
+
+
+# Define constants.
+n_thresholds = 100
+data_dir = localmodule.get_data_dir()
+dataset_name = localmodule.get_dataset_name()
+models_dir = localmodule.get_models_dir()
+args = sys.argv[1:]
+unit_str = args[0]
+odf_str = args[1]
+
+
+# Print header.
+start_time = int(time.time())
+print(str(datetime.datetime.now()) + " Start.")
+print("Thresholding spectral flux on " + dataset_name + ", " + unit_str + ".")
+print('h5py version: {:s}.'.format(h5py.__version__))
+print('numpy version: {:s}'.format(np.__version__))
+print("")
+
+
+# Print elapsed time.
+print(str(datetime.datetime.now()) + " Finish.")
+elapsed_time = time.time() - int(start_time)
+elapsed_hours = int(elapsed_time / (60 * 60))
+elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
+elapsed_seconds = elapsed_time % 60.
+elapsed_str = "{:>02}:{:>02}:{:>05.2f}".format(elapsed_hours,
+                                               elapsed_minutes,
+                                               elapsed_seconds)
+print("Total elapsed time: " + elapsed_str + ".")
