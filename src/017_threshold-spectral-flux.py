23bd3ebcd22d20e858b5f51aca85e1acd858dfa7
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-21 20:02:32 -0400

    odf_path = os.path.join(sf_dir, unit_str + ".hdf5")

--- src/017_threshold-spectral-flux.py
+++ src/017_threshold-spectral-flux.py
@@ -30,7 +30,7 @@ print("")
 # Load onset detection function.
 sf_name = "_".join([dataset_name, "spectral-flux"])
 sf_dir = os.path.join(data_dir, sf_name)
-odf_path = os.path.join(oldbird_dir, unit_str + ".hdf5")
+odf_path = os.path.join(sf_dir, unit_str + ".hdf5")
 odf_file = h5py.File(odf_path, "r")
 odf_dataset_key = "spectral-flux_odf"
 odf = odf_file[odf_dataset_key]
