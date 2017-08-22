90735ca49ae7586b8e9050d33de426da0788675d
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-21 20:04:40 -0400

    define threshold_id_range

--- src/017_threshold-spectral-flux.py
+++ src/017_threshold-spectral-flux.py
@@ -16,6 +16,9 @@ dataset_name = localmodule.get_dataset_name()
 models_dir = localmodule.get_models_dir()
 args = sys.argv[1:]
 unit_str = args[0]
+threshold_id_start = int(args[1][3:5])
+threshold_id_stop = int(args[1][-2:])
+threshold_id_range = range(threshold_id_start, 1 + threshold_id_stop)
 
 
 # Print header.
