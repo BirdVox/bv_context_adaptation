117c6097eeb9115cf43679603db2d9737b894f52
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-25 12:52:23 -0400

    update 014

--- src/014_predict-icassp-convnet-clips.py
+++ src/014_predict-icassp-convnet-clips.py
@@ -38,8 +38,8 @@ validation_units = fold[2]
 # Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start.")
-print("Using Salamon's ICASSP 2017 convnet for binary classigification in " +
-    dataset_name + ". ")
+print("Using Salamon's ICASSP 2017 convnet for binary classification in " +
+    dataset_name + " clips. ")
 print("Training set: " + ", ".join(training_units) + ".")
 print("Validation set: " + ", ".join(validation_units) + ".")
 print("Test set: " + ", ".join(test_units) + ".")
