ef89c6b413ddd6afca7eeed59f492e7790f9eb34
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-25 12:52:27 -0400

    update 015

--- src/015_predict-icassp-convnet-full-audio.py
+++ src/015_predict-icassp-convnet-full-audio.py
@@ -4,6 +4,7 @@ import h5py
 import keras
 import numpy as np
 import os
+import pandas as pd
 import sys
 import tensorflow as tf
 import time
@@ -37,8 +38,8 @@ validation_units = fold[2]
 # Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start.")
-print("Using Salamon's ICASSP 2017 convnet for binary classigification in " +
-    dataset_name + ". ")
+print("Using Salamon's ICASSP 2017 convnet for binary classification in " +
+    dataset_name + ", full audio. ")
 print("Training set: " + ", ".join(training_units) + ".")
 print("Validation set: " + ", ".join(validation_units) + ".")
 print("Test set: " + ", ".join(test_units) + ".")
@@ -58,21 +59,86 @@ model_dir = os.path.join(models_dir, model_name)
 unit_dir = os.path.join(model_dir, test_unit_str)
 trial_dir = os.path.join(unit_dir, trial_str)
 network_name = "_".join(
-    [dataset_name, model_name, aug_kind_str,
-     test_unit_str, trial_str, "network"])
+    [dataset_name, model_name, test_unit_str, trial_str, "network"])
 network_path = os.path.join(trial_dir, network_name + ".hdf5")
 model = keras.models.load_model(network_path)
 
 
 # Open logmelspec container with h5py.
 aug_str = "original"
-logmelspec_dir = os.path.join(data_dir,
-    "_".join([dataset_name, "full-logmelspec"]))
+logmelspec_dir = os.path.join(data_dir, "_".join([dataset_name, "logmelspec"]))
 original_logmelspec_dir = os.path.join(logmelspec_dir, aug_str)
 hdf5_name = "_".join([dataset_name, aug_str, predict_unit_str])
 hdf5_path = os.path.join(original_logmelspec_dir, hdf5_name + ".hdf5")
 lms_container = h5py.File(hdf5_path, "r")
 lms_group = lms_container["logmelspec"]
+
+
+# Create HDF5 container for predictions.
+clip_predictions_name = "_".join([
+    dataset_name,
+    model_name,
+    "test-" + test_unit_str,
+    "predict-" + predict_unit_str,
+    "clip-predictions"
+])
+
+
+# List keys.
+keys = sorted(list(lms_group.keys()))
+
+
+# Create CSV file.
+prediction_name = "_".join([dataset_name, model_name,
+    "test-" + test_unit_str, trial_str, "predict-" + predict_unit_str,
+    "clip-predictions"])
+prediction_path = os.path.join(trial_dir, prediction_name + ".csv")
+csv_file = open(prediction_path, 'w')
+csv_writer = csv.writer(csv_file, delimiter=',')
+
+
+# Create CSV header.
+csv_header = ["Dataset", "Test unit", "Prediction unit", "Timestamp",
+    "Center frequency (Hz)", "Augmentation", "Key", "Ground truth",
+    "Predicted probability"]
+csv_writer.writerow(csv_header)
+
+
+# Loop over keys.
+for key in keys:
+    # Load logmelspec.
+    X = lms_group[key]
+
+    # Trim logmelspec in time to required number of hops.
+    X_width = X.shape[1]
+    first_col = int((X_width-n_input_hops) / 2)
+    last_col = int((X_width+n_input_hops) / 2)
+    X = X[:, first_col:last_col]
+
+    # Add trailing singleton dimension for Keras interoperability.
+    X = X[np.newaxis, :, :, np.newaxis]
+
+    # Predict.
+    predicted_probability = model.predict(X)[0, 0]
+
+    # Store prediction as DataFrame row.
+    key_split = key.split("_")
+    timestamp_str = key_split[1]
+    freq_str = key_split[2]
+    ground_truth_str = key_split[3]
+    aug_str = key_split[4]
+    predicted_probability_str = "{:.16f}".format(predicted_probability)
+    row = [dataset_name, test_unit_str, predict_unit_str, timestamp_str,
+         freq_str, aug_str, key, ground_truth_str, predicted_probability_str]
+    csv_writer.writerow(row)
+
+
+# Close HDF5 containers.
+lms_container.close()
+
+
+# Close CSV file.
+csv_file.close()
 
 
 # Print elapsed time.
