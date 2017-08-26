51c4e68fe7d5a68ed2d9ff9bbae2550019c8c353
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-26 15:37:12 -0400

    generate 018 shell files

---
+++ sbatch/018_aug-none.sh
@@ -0,0 +1,6 @@
+# This shell script executes Slurm jobs for evaluating
+# Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k clips.
+# Augmentation kind: none.
+
+sbatch 018_aug-none.sbatch
---
+++ sbatch/018_aug-stretch.sh
@@ -0,0 +1,6 @@
+# This shell script executes Slurm jobs for evaluating
+# Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k clips.
+# Augmentation kind: stretch.
+
+sbatch 018_aug-stretch.sbatch
