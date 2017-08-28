0cef7a10e684e4e24209d64ff943a6455347c450
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-28 15:36:08 -0400

    update generate_014-sbatch

--- sbatch/generate_014-sbatch.py
+++ sbatch/generate_014-sbatch.py
@@ -16,6 +16,14 @@ script_path = os.path.join("..", "..", "..", "src", script_name)
 script_str = script_name[:3]
 
 
+# Create folders.
+os.makedirs(script_str, exist_ok=True)
+sbatch_dir = os.path.join(script_str, "sbatch")
+os.makedirs(sbatch_dir, exist_ok=True)
+slurm_dir = os.path.join(script_str, "slurm")
+os.makedirs(slurm_dir, exist_ok=True)
+
+
 # Loop over kind of augmentations
 for aug_kind_str in aug_kinds:
 
@@ -40,12 +48,18 @@ for aug_kind_str in aug_kinds:
                     trial_str,
                     "predict-" + predict_unit_str])
                 file_name = job_name + ".sbatch"
+                file_path = os.path.join(sbatch_dir, file_name)
                 script_path_with_args = " ".join([
                     script_path,
                     aug_kind_str,
                     test_unit_str,
                     trial_str,
                     predict_unit_str])
+
+                # Define slurm name.
+                slurm_prefix = "../slurm/slurm"
+                slurm_suffix = "%j"
+                slurm_name = "_".join(slurm_prefix, job_name, slurm_suffix)
 
                 # Write call to python in SBATCH file.
                 with open(file_name, "w") as f:
@@ -57,7 +71,7 @@ for aug_kind_str in aug_kinds:
                     f.write("#SBATCH --cpus-per-task=1\n")
                     f.write("#SBATCH --time=0:10:00\n")
                     f.write("#SBATCH --mem=500MB\n")
-                    f.write("#SBATCH --output=slurm_" + job_name + "_%j.out\n")
+                    f.write("#SBATCH --output=" + slurm_name + ".out\n")
                     f.write("\n")
                     f.write("module purge\n")
                     f.write("module load cuda/8.0.44\n")
