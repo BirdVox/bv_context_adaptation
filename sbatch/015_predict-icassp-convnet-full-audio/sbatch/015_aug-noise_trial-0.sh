2f8c90216c91fd55944ffcb64eb4a014ecb677ef
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-28 16:25:36 -0400

    generate 015 shell files

---
+++ sbatch/015_predict-icassp-convnet-full-audio/sbatch/015_aug-noise_trial-0.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k, full audio.
+# Trial ID: 0.
+# Augmentation kind: noise.
+
+sbatch 015_aug-noise_test-unit01_trial-0_predict-unit07.sbatch
+sbatch 015_aug-noise_test-unit01_trial-0_predict-unit10.sbatch
+sbatch 015_aug-noise_test-unit01_trial-0_predict-unit01.sbatch
+
+sbatch 015_aug-noise_test-unit02_trial-0_predict-unit10.sbatch
+sbatch 015_aug-noise_test-unit02_trial-0_predict-unit01.sbatch
+sbatch 015_aug-noise_test-unit02_trial-0_predict-unit02.sbatch
+
+sbatch 015_aug-noise_test-unit03_trial-0_predict-unit01.sbatch
+sbatch 015_aug-noise_test-unit03_trial-0_predict-unit02.sbatch
+sbatch 015_aug-noise_test-unit03_trial-0_predict-unit03.sbatch
+
+sbatch 015_aug-noise_test-unit05_trial-0_predict-unit02.sbatch
+sbatch 015_aug-noise_test-unit05_trial-0_predict-unit03.sbatch
+sbatch 015_aug-noise_test-unit05_trial-0_predict-unit05.sbatch
+
+sbatch 015_aug-noise_test-unit07_trial-0_predict-unit03.sbatch
+sbatch 015_aug-noise_test-unit07_trial-0_predict-unit05.sbatch
+sbatch 015_aug-noise_test-unit07_trial-0_predict-unit07.sbatch
+
+sbatch 015_aug-noise_test-unit10_trial-0_predict-unit05.sbatch
+sbatch 015_aug-noise_test-unit10_trial-0_predict-unit07.sbatch
+sbatch 015_aug-noise_test-unit10_trial-0_predict-unit10.sbatch
+
---
+++ sbatch/015_predict-icassp-convnet-full-audio/sbatch/015_aug-noise_trial-3.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k, full audio.
+# Trial ID: 3.
+# Augmentation kind: noise.
+
+sbatch 015_aug-noise_test-unit01_trial-3_predict-unit07.sbatch
+sbatch 015_aug-noise_test-unit01_trial-3_predict-unit10.sbatch
+sbatch 015_aug-noise_test-unit01_trial-3_predict-unit01.sbatch
+
+sbatch 015_aug-noise_test-unit02_trial-3_predict-unit10.sbatch
+sbatch 015_aug-noise_test-unit02_trial-3_predict-unit01.sbatch
+sbatch 015_aug-noise_test-unit02_trial-3_predict-unit02.sbatch
+
+sbatch 015_aug-noise_test-unit03_trial-3_predict-unit01.sbatch
+sbatch 015_aug-noise_test-unit03_trial-3_predict-unit02.sbatch
+sbatch 015_aug-noise_test-unit03_trial-3_predict-unit03.sbatch
+
+sbatch 015_aug-noise_test-unit05_trial-3_predict-unit02.sbatch
+sbatch 015_aug-noise_test-unit05_trial-3_predict-unit03.sbatch
+sbatch 015_aug-noise_test-unit05_trial-3_predict-unit05.sbatch
+
+sbatch 015_aug-noise_test-unit07_trial-3_predict-unit03.sbatch
+sbatch 015_aug-noise_test-unit07_trial-3_predict-unit05.sbatch
+sbatch 015_aug-noise_test-unit07_trial-3_predict-unit07.sbatch
+
+sbatch 015_aug-noise_test-unit10_trial-3_predict-unit05.sbatch
+sbatch 015_aug-noise_test-unit10_trial-3_predict-unit07.sbatch
+sbatch 015_aug-noise_test-unit10_trial-3_predict-unit10.sbatch
+
