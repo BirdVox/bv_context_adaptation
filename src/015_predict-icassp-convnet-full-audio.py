bd4743c3be48c17b248534431d87ddb77cb7b1a5
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-08-28 16:11:12 -0400

    update 015 script

--- src/015_predict-icassp-convnet-full-audio.py
+++ src/015_predict-icassp-convnet-full-audio.py
@@ -9,6 +9,7 @@ import sys
 import tensorflow as tf
 import time
 
+sys.path.append("../src")
 import localmodule
 
 
@@ -17,11 +18,16 @@ data_dir = localmodule.get_data_dir()
 dataset_name = localmodule.get_dataset_name()
 folds = localmodule.fold_units()
 models_dir = localmodule.get_models_dir()
-n_input_hops = 104
+model_name = "icassp-convnet"
+if not aug_kind_str == "none":
+    model_name = "_".join([model_name, "aug-" + aug_kind_str])
+clip_length = 104
+hop_length = int(np.round(clip_length / 2))
+hop_duration = hop_length * 32 / 22050
 
 
 # Read command-line arguments.
-args = sys.argv[1:]
+args = ["none", "unit01", "trial-0", "unit01"]
 aug_kind_str = args[0]
 test_unit_str = args[1]
 trial_str = args[2]
@@ -82,51 +88,46 @@ clip_predictions_name = "_".join([
 ])
 
 
-# List keys.
-keys = sorted(list(lms_group.keys()))
-
-
 # Create CSV file.
 prediction_name = "_".join([dataset_name, model_name,
     "test-" + test_unit_str, trial_str, "predict-" + predict_unit_str,
-    "clip-predictions"])
+    "full-predictions"])
 prediction_path = os.path.join(trial_dir, prediction_name + ".csv")
 csv_file = open(prediction_path, 'w')
 csv_writer = csv.writer(csv_file, delimiter=',')
 
 
 # Create CSV header.
-csv_header = ["Dataset", "Test unit", "Prediction unit", "Timestamp",
-    "Key", "Ground truth", "Predicted probability"]
+csv_header = ["Dataset", "Model", "Test unit", "Prediction unit", "Timestamp",
+    "Predicted probability"]
 csv_writer.writerow(csv_header)
 
 
-# Loop over keys.
-for key in keys:
-    # Load logmelspec.
-    X = lms_group[key]
+# Compute number of hops.
+n_hops = int(lms_group.shape[1] / hop_length) - 1
 
-    # Trim logmelspec in time to required number of hops.
-    X_width = X.shape[1]
-    first_col = int((X_width-n_input_hops) / 2)
-    last_col = int((X_width+n_input_hops) / 2)
-    X = X[:, first_col:last_col]
 
-    # Add trailing singleton dimension for Keras interoperability.
+# Loop over hops.
+for hop_id in range(n_hops)[:100]:
+
+    # Load clip in full logmelspec data.
+    clip_start = hop_id * hop_length
+    clip_stop = clip_start + clip_length
+    X = lms_group[:, clip_start:clip_stop]
+
+    # Add leading and trailing singleton dimension for Keras interoperability.
     X = X[np.newaxis, :, :, np.newaxis]
 
     # Predict.
-    predicted_probability = model.predict(X)[0, 0]
+    predicted_probability = model.predict(X)[0,0]
 
     # Store prediction as DataFrame row.
+    timestamp = (hop_id+1) * hop_duration
+    timestamp_str = "{:9.3f}".format(timestamp)
     predicted_probability_str = "{:.16f}".format(predicted_probability)
-    row = [dataset_name, test_unit_str, predict_unit_str, timestamp_str,
-        key, predicted_probability_str]
+    row = [dataset_name, model_name, test_unit_str, predict_unit_str,
+        timestamp_str, predicted_probability_str]
     csv_writer.writerow(row)
-
-
-# Close HDF5 containers.
-lms_container.close()
 
 
 # Close CSV file.
