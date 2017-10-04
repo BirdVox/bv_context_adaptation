4f2358882d34ae7d5bd009afa445eec940fde3c9
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-04 14:56:35 -0400

    set Q=24, NFO=48

--- src/compute_snowball.m
+++ src/compute_snowball.m
@@ -1,20 +1,22 @@
 hdf5_path = ['/beegfs/vl1019/spl2017_data/BirdVox-70k_hdf5/' ...
     'original/BirdVox-70k_original_unit01.hdf5'];
-Q = 16;
+addpath(genpath('~/scattering.m'));
 
 % Define options for first-order scattering (time-frequency representation).
 opts{1}.time.size = 8192;
 % Temporal oversampling by a factor 2^1=2.
 opts{1}.time.U_log2_oversampling = 1;
 % Number of filters per octave.
-opts{1}.time.nFilters_per_octave = Q;
+opts{1}.time.nFilters_per_octave = 48;
+% Maximum quality factor.
+opts{1}.time.max_Q = 24;
 % Support of the low-pass filter phi.
-opts{1}.time.T = 256;
+opts{1}.time.T = 8;
 % Hard constraint on the maximum scale of the wavelets psi.
 opts{1}.time.max_scale = 2048;
 % Number of octaves in the wavelet filterbank.
 opts{1}.time.nOctaves = 8;
-opts{1}.time.gamma_bounds = [1 (1+8*Q)];
+opts{1}.time.gamma_bounds = [1 128];
 % Disable chunking
 opts{1}.time.is_chunked = false;
 
@@ -23,17 +25,15 @@ opts{2}.invariants.time.T = 32;
 
 % Define options for second-order scattering along time.
 opts{2}.banks.time.T = 8192;
-% Gammatone wavelets (asymmetric) instead of Morlet (symmetric).
-opts{2}.banks.time.handle = @gammatone_1d;
 % No constraint on the maximum scale.
 opts{2}.banks.time.max_scale = Inf;
-% Only retain modulation frequencies below 64 Hz.
-opts{2}.banks.time.gamma_bounds = [10 Inf];
 opts{2}.banks.time.sibling_mask_factor = Inf;
 
 % No subsampling along log-frequencies.
 opts{2}.banks.gamma.U_log2_oversampling = Inf;
 opts{2}.banks.gamma.S_log2_oversampling = Inf;
+
+archs = sc_setup(opts);
 
 
 addpath(genpath('~/scattering.m'));
@@ -51,8 +51,6 @@ sample_rate = h5read(hdf5_path, '/sample_rate');
 % Print header.
 disp(['Computing scattering transforms for ', ...
     dataset_name, ' augmented data.']);
-Q_str = sprintf('%02d', Q);
-disp(['Quality factor (Q): ', Q_str, '.']);
 disp(['Unit: ', unit_str, '.'])
 disp(['Augmentation: ', instanced_aug_str, '.']);
 disp(['Number of files: ', sprintf('%d', n_waveform_names), '.']);
@@ -64,3 +62,5 @@ waveform_path = ['/waveforms/', waveform_name];
 
 waveform = h5read(hdf5_path, waveform_path);
 waveform = waveform(1:8192);
+
+[S, U] = sc_propagate(waveform, archs);
