7938800e7a97d4b9d63cdae1b8c73c175e497aa5
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-10 07:17:27 +0530

    define min_dist as args[1]

--- src/017_threshold-spectral-flux.py
+++ src/017_threshold-spectral-flux.py
@@ -3,6 +3,7 @@ import datetime
 import h5py
 import numpy as np
 import os
+import peakutils
 import sys
 import time
 
@@ -16,9 +17,7 @@ dataset_name = localmodule.get_dataset_name()
 models_dir = localmodule.get_models_dir()
 args = sys.argv[1:]
 unit_str = args[0]
-threshold_id_start = int(args[1][3:5])
-threshold_id_stop = int(args[1][-2:])
-threshold_id_range = range(threshold_id_start, 1 + threshold_id_stop)
+min_dist = int(args[1])
 
 
 # Print header.
@@ -75,6 +74,13 @@ csv_header = [
     'Threshold',
     'Time (s)',
     'Onset ODF']
+
+
+
+# Loop over thresholds.
+for threshold in thresholds:
+    peak_locations = peakutils.indexes(odf,
+        thres=threshold, min_dist=min_dist)
 
 
 # Close HDF5 file.
