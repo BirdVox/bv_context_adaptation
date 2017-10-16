81d5d2c8420e3f07b9ef4637da27d3ce60732f1f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-16 13:11:46 -0400

    update job_name

--- sbatch/generate_020-shell.py
+++ sbatch/generate_020-shell.py
@@ -47,7 +47,7 @@ with open(file_path, "w") as f:
             # Loop over recording units.
             for unit_str in units:
                 # Define job name.
-                job_name = "_".join(["005", instanced_aug_str, unit_str])
+                job_name = "_".join(["020", instanced_aug_str, unit_str])
                 sbatch_str = "sbatch " + job_name + ".sbatch"
 
                 # Write SBATCH command to shell file.
