e31760af290941b0da8427ede099ef2755a183d9
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-18 11:36:49 -0400

    up

--- sbatch/generate_020-shell.py
+++ sbatch/generate_020-shell.py
@@ -20,45 +20,42 @@ slurm_dir = os.path.join(script_dir, "slurm")
 os.makedirs(slurm_dir, exist_ok=True)
 
 
-# Define file path.
-file_path = os.path.join(sbatch_dir, script_name[:3] + ".sh")
-
-
-
 # Loop over augmentations.
 for aug_str in augmentations:
+    # Define shell path.
+    shell_name = script_name[:3] + "_" + aug_str + ".sh"
+    shell_path = os.path.join(sbatch_dir, shell_name)
 
-    # Loop over instances.
-    n_instances = augmentations[aug_str]
-    for instance_id in range(n_instances):
-        instance_str = str(instance_id)
+    # Open shell file.
+    with open(shell_path, "w") as f:
+        # Print header.
+        f.write("# This shell script executes the Slurm jobs for " +\
+            "computing log-mel-spectrograms for SKM-SVM model.\n")
+        f.write("\n")
 
-        if aug_str == "original":
-            instanced_aug_str = aug_str
-        else:
-            instanced_aug_str = "_".join([aug_str, instance_str])
+        # Loop over instances.
+        n_instances = augmentations[aug_str]
+        for instance_id in range(n_instances):
+            instance_str = str(instance_id)
 
-        # Open shell file.
-        with open(file_path, "w") as f:
-            # Print header.
-            f.write("# This shell script executes the Slurm jobs for computing" +\
-                "log-mel-spectrograms for SKM-SVM model.\n")
+            if aug_str == "original":
+                instanced_aug_str = aug_str
+            else:
+                instanced_aug_str = "_".join([aug_str, instance_str])
+
+                # Loop over recording units.
+                for unit_str in units:
+                    # Define job name.
+                    job_name = "_".join(["020", instanced_aug_str, unit_str])
+                    sbatch_str = "sbatch " + job_name + ".sbatch"
+
+                    # Write SBATCH command to shell file.
+                    f.write(sbatch_str + "\n")
             f.write("\n")
 
-            # Loop over recording units.
-            for unit_str in units:
-                # Define job name.
-                job_name = "_".join(["020", instanced_aug_str, unit_str])
-                sbatch_str = "sbatch " + job_name + ".sbatch"
 
-                # Write SBATCH command to shell file.
-                f.write(sbatch_str + "\n")
-        f.write("\n")
-    f.write("\n")
-
-
-# Grant permission to execute the shell file.
-# https://stackoverflow.com/a/30463972
-mode = os.stat(file_path).st_mode
-mode |= (mode & 0o444) >> 2
-os.chmod(file_path, mode)
+    # Grant permission to execute the shell file.
+    # https://stackoverflow.com/a/30463972
+    mode = os.stat(shell_path).st_mode
+    mode |= (mode & 0o444) >> 2
+    os.chmod(file_path, mode)
