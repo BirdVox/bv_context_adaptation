91305d589cd8ec21300b34e87cd6d3f1d4040be0
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-18 12:46:59 -0400

    generate 021.sh

---
+++ sbatch/021_compute-skm-full-logmelspec/sbatch/021.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing log-mel-spectrograms on continuous audio.
+
+sbatch 021_unit01.sbatch
+sbatch 021_unit02.sbatch
+sbatch 021_unit03.sbatch
+sbatch 021_unit05.sbatch
+sbatch 021_unit07.sbatch
+sbatch 021_unit10.sbatch
