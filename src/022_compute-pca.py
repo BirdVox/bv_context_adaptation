435344ae3df98dd54767d0282ffb95e61ea3178f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-19 11:50:21 -0400

    update 022

--- src/022_compute-pca.py
+++ src/022_compute-pca.py
@@ -3,7 +3,7 @@ import h5py
 import librosa
 import numpy as np
 import os
-import pandas as pd
+import sklearn
 import soundfile as sf
 import sys
 import time
@@ -15,7 +15,14 @@ import localmodule
 data_dir = localmodule.get_data_dir()
 dataset_name = localmodule.get_dataset_name()
 args = sys.argv[1:]
-unit_str = args[0]
+aug_str = args[0]
+instance_id = int(args[1])
+instance_str = str(instance_id)
+unit_str = args[2]
+if aug_str == "original":
+    instanced_aug_str = aug_str
+else:
+    instanced_aug_str = "-".join([aug_str, instance_str])
 
 
 # Print header.
@@ -29,6 +36,22 @@ print("librosa version: {:s}".format(librosa.__version__))
 print("numpy version: {:s}".format(np.__version__))
 print("scikit-learn version: {:s}".format(sklearn.__version__))
 print("")
+
+
+# Load HDF5 container of logmelspecs.
+logmelspec_name = "_".join([dataset_name, "skm-logmelspec"])
+logmelspec_dir = os.path.join(data_dir, logmelspec_name)
+aug_dir = os.path.join(logmelspec_dir, aug_str)
+in_path = os.path.join(aug_dir, hdf5_name + ".hdf5")
+in_file = h5py.File(in_path)
+
+
+# List keys.
+
+
+
+# Close HDF5 file.
+in_file.close()
 
 
 # Print elapsed time.
