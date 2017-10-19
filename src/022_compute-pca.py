5d57a1f7e7c7129d0ffac359adce2948477703f4
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-19 12:10:12 -0400

    loop over time-freq patches

--- src/022_compute-pca.py
+++ src/022_compute-pca.py
@@ -86,6 +86,9 @@ for train_unit_str in training_units:
         logmelspec_start = logmelspec_mid -\
             np.round(patch_width * n_patches_per_clip * 0.5).astype('int')
 
+        # Loop over time-frequency patches.
+        for patch_id in range(n_patches_per_clip):
+
 
 
 # Close HDF5 file.
