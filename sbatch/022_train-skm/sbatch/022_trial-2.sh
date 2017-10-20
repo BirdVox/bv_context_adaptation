0ca7985ae8c4dcb9ae383c1ff13b692a83862d1b
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-20 16:38:57 -0400

    re-genreate 022 shell files

--- sbatch/022_train-skm/sbatch/022_trial-2.sh
+++ sbatch/022_train-skm/sbatch/022_trial-2.sh
@@ -1,8 +1,8 @@
 # This shell script executes the Slurm jobs for training Salamon's unsupervised learning pipeline (PCA-SKM).
 
-sbatch 021_unit01_trial-2.sbatch
-sbatch 021_unit02_trial-2.sbatch
-sbatch 021_unit03_trial-2.sbatch
-sbatch 021_unit05_trial-2.sbatch
-sbatch 021_unit07_trial-2.sbatch
-sbatch 021_unit10_trial-2.sbatch
+sbatch 022_unit01_trial-2.sbatch
+sbatch 022_unit02_trial-2.sbatch
+sbatch 022_unit03_trial-2.sbatch
+sbatch 022_unit05_trial-2.sbatch
+sbatch 022_unit07_trial-2.sbatch
+sbatch 022_unit10_trial-2.sbatch
