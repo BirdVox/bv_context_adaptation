adc659f9104e562a441bc16d71ba6a6345422cd9
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-10-26 03:42:24 -0400

    generate 024 sbatch and shell files

---
+++ sbatch/024_train-probabilistic-svm/sbatch/024_trial-4.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for training Salamon's supervised learning pipeline (SVM) in probabilistic mode. It also predicts on the test set.
+
+sbatch 024_unit01_trial-4.sbatch
+sbatch 024_unit02_trial-4.sbatch
+sbatch 024_unit03_trial-4.sbatch
+sbatch 024_unit05_trial-4.sbatch
+sbatch 024_unit07_trial-4.sbatch
+sbatch 024_unit10_trial-4.sbatch
