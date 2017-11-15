86ea3b789d0a15ea3bc231a45935902c4d5b8bb5
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-15 18:20:48 -0500

    print footer in 026

--- src/026_compute-clip-background-summaries.py
+++ src/026_compute-clip-background-summaries.py
@@ -117,3 +117,15 @@ for in_clip_key in in_clip_keys:
 out_unit_file.close()
 in_full_unit_file.close()
 in_clip_unit_file.close()
+
+
+# Print footer.
+print(str(datetime.datetime.now()) + " Finish.")
+elapsed_time = time.time() - int(start_time)
+elapsed_hours = int(elapsed_time / (60 * 60))
+elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
+elapsed_seconds = elapsed_time % 60.
+elapsed_str = "{:>02}:{:>02}:{:>05.2f}".format(elapsed_hours,
+                                               elapsed_minutes,
+                                               elapsed_seconds)
+print("Total elapsed time: " + elapsed_str + ".")
