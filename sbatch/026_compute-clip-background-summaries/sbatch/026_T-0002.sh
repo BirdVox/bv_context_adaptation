c60887822a2bfc2e09671b243a5ed76ff18365ca
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-15 18:11:57 -0500

    generate 026 shell and sbatch files

---
+++ sbatch/026_compute-clip-background-summaries/sbatch/026_T-0002.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing backgrounds on clips.
+
+sbatch 026_T-0002_unit01.sbatch
+sbatch 026_T-0002_unit02.sbatch
+sbatch 026_T-0002_unit03.sbatch
+sbatch 026_T-0002_unit05.sbatch
+sbatch 026_T-0002_unit07.sbatch
+sbatch 026_T-0002_unit10.sbatch
---
+++ sbatch/026_compute-clip-background-summaries/sbatch/026_T-0600.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing backgrounds on clips.
+
+sbatch 026_T-0600_unit01.sbatch
+sbatch 026_T-0600_unit02.sbatch
+sbatch 026_T-0600_unit03.sbatch
+sbatch 026_T-0600_unit05.sbatch
+sbatch 026_T-0600_unit07.sbatch
+sbatch 026_T-0600_unit10.sbatch
