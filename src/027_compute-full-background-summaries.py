3f856f15545bb525ffa93ffffada28d4a691c4a7
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-20 10:26:48 -0500

    up

--- src/027_compute-full-background-summaries.py
+++ src/027_compute-full-background-summaries.py
@@ -84,8 +84,9 @@ out_lms_group = out_unit_file.create_dataset(
 
 
 # Load over clips.
-print(n_bg_hops)
-for bg_hop_id in range(n_bg_hops)[:1]:
+print(n_bg_hops) # UPDATE ME
+start_time = int(time.time()) #UPDATE ME
+for bg_hop_id in range(n_bg_hops)[:10]: # UPDATE ME
 
     # Load background excerpt.
     lms_start = bg_hop_id * bg_hop
@@ -106,7 +107,7 @@ out_unit_file.close()
 
 # Print footer.
 print(str(datetime.datetime.now()) + " Finish.")
-elapsed_time = time.time() - int(start_time)
+elapsed_time = (time.time() - int(start_time)) * (n_bg_hops/10) # UPDATE ME
 elapsed_hours = int(elapsed_time / (60 * 60))
 elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
 elapsed_seconds = elapsed_time % 60.
