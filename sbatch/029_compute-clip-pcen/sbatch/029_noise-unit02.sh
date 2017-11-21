a95805ec4e06f3b7a0ec065e9a75b174e51a7507
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-21 15:55:00 -0500

    generate 029 shell files

---
+++ sbatch/029_compute-clip-pcen/sbatch/029_noise-unit02.sh
@@ -0,0 +1,31 @@
+# This shell script executes the Slurm jobs for computing clip-level PCEN.
+
+sbatch 029_noise-unit02_0_unit01.sbatch
+sbatch 029_noise-unit02_0_unit02.sbatch
+sbatch 029_noise-unit02_0_unit03.sbatch
+sbatch 029_noise-unit02_0_unit05.sbatch
+sbatch 029_noise-unit02_0_unit07.sbatch
+sbatch 029_noise-unit02_0_unit10.sbatch
+
+sbatch 029_noise-unit02_1_unit01.sbatch
+sbatch 029_noise-unit02_1_unit02.sbatch
+sbatch 029_noise-unit02_1_unit03.sbatch
+sbatch 029_noise-unit02_1_unit05.sbatch
+sbatch 029_noise-unit02_1_unit07.sbatch
+sbatch 029_noise-unit02_1_unit10.sbatch
+
+sbatch 029_noise-unit02_2_unit01.sbatch
+sbatch 029_noise-unit02_2_unit02.sbatch
+sbatch 029_noise-unit02_2_unit03.sbatch
+sbatch 029_noise-unit02_2_unit05.sbatch
+sbatch 029_noise-unit02_2_unit07.sbatch
+sbatch 029_noise-unit02_2_unit10.sbatch
+
+sbatch 029_noise-unit02_3_unit01.sbatch
+sbatch 029_noise-unit02_3_unit02.sbatch
+sbatch 029_noise-unit02_3_unit03.sbatch
+sbatch 029_noise-unit02_3_unit05.sbatch
+sbatch 029_noise-unit02_3_unit07.sbatch
+sbatch 029_noise-unit02_3_unit10.sbatch
+
+
