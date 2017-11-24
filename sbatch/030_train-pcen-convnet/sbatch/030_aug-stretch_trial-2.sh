3051ea7904175db9ec382c0fc3a22edc19247354
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-24 15:36:22 -0500

    generate 030 shell files

---
+++ sbatch/030_train-pcen-convnet/sbatch/030_aug-stretch_trial-2.sh
@@ -0,0 +1,13 @@
+# This shell script executes Slurm jobs for training
+# Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k with PCEN input instead of
+# log-mel-spectrogram (logmelspec) input.
+# Trial ID: 2.
+# Augmentation kind: stretch.
+
+sbatch 030_aug-stretch_unit01_trial-2.sbatch
+sbatch 030_aug-stretch_unit02_trial-2.sbatch
+sbatch 030_aug-stretch_unit03_trial-2.sbatch
+sbatch 030_aug-stretch_unit05_trial-2.sbatch
+sbatch 030_aug-stretch_unit07_trial-2.sbatch
+sbatch 030_aug-stretch_unit10_trial-2.sbatch
