3cd2466d7b96e0b2384492794e151a866cdc068e
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-26 13:23:17 -0500

    generate 032.sh

---
+++ sbatch/032_compute-full-pcen/sbatch/032.sh
@@ -0,0 +1,9 @@
+# This shell script executes the Slurm jobs for computing log-mel-spectrograms on continuous audio.
+
+sbatch 032_unit01.sbatch
+sbatch 032_unit02.sbatch
+sbatch 032_unit03.sbatch
+sbatch 032_unit05.sbatch
+sbatch 032_unit07.sbatch
+sbatch 032_unit10.sbatch
+
