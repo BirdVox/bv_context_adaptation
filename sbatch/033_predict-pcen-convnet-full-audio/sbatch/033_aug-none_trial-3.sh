70e7887ffa0bd9dcca930da9bf70bcdd4b69410a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-26 14:24:29 -0500

    generate 033 shell files

---
+++ sbatch/033_predict-pcen-convnet-full-audio/sbatch/033_aug-none_trial-3.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k, full audio, with PCEN input.
+# Trial ID: 3.
+# Augmentation kind: none.
+
+sbatch 033_aug-none_test-unit01_trial-3_predict-unit07.sbatch
+sbatch 033_aug-none_test-unit01_trial-3_predict-unit10.sbatch
+sbatch 033_aug-none_test-unit01_trial-3_predict-unit01.sbatch
+
+sbatch 033_aug-none_test-unit02_trial-3_predict-unit10.sbatch
+sbatch 033_aug-none_test-unit02_trial-3_predict-unit01.sbatch
+sbatch 033_aug-none_test-unit02_trial-3_predict-unit02.sbatch
+
+sbatch 033_aug-none_test-unit03_trial-3_predict-unit01.sbatch
+sbatch 033_aug-none_test-unit03_trial-3_predict-unit02.sbatch
+sbatch 033_aug-none_test-unit03_trial-3_predict-unit03.sbatch
+
+sbatch 033_aug-none_test-unit05_trial-3_predict-unit02.sbatch
+sbatch 033_aug-none_test-unit05_trial-3_predict-unit03.sbatch
+sbatch 033_aug-none_test-unit05_trial-3_predict-unit05.sbatch
+
+sbatch 033_aug-none_test-unit07_trial-3_predict-unit03.sbatch
+sbatch 033_aug-none_test-unit07_trial-3_predict-unit05.sbatch
+sbatch 033_aug-none_test-unit07_trial-3_predict-unit07.sbatch
+
+sbatch 033_aug-none_test-unit10_trial-3_predict-unit05.sbatch
+sbatch 033_aug-none_test-unit10_trial-3_predict-unit07.sbatch
+sbatch 033_aug-none_test-unit10_trial-3_predict-unit10.sbatch
+
