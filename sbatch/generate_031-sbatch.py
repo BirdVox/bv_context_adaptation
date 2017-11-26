7055c807c76889e4394bd522c82f6513f46543ef
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-26 12:07:49 -0500

    set time to 10 minutes in 031

--- sbatch/generate_031-sbatch.py
+++ sbatch/generate_031-sbatch.py
@@ -70,7 +70,7 @@ for aug_kind_str in aug_kinds:
                     f.write("#SBATCH --nodes=1\n")
                     f.write("#SBATCH --tasks-per-node=1\n")
                     f.write("#SBATCH --cpus-per-task=1\n")
-                    f.write("#SBATCH --time=4:00:00\n")
+                    f.write("#SBATCH --time=0:10:00\n")
                     f.write("#SBATCH --mem=500MB\n")
                     f.write("#SBATCH --output=" + slurm_name + "\n")
                     f.write("\n")
