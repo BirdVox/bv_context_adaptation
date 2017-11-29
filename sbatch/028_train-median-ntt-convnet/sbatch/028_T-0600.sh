adcff5632b4ce45787d70771f185e5de8ae13428
Vincent <vincent.lostanlen@nyu.edu>
2017-11-29 11:34:19 -0500

    up

--- sbatch/028_train-median-ntt-convnet/sbatch/028_T-0600.sh
+++
@@ -1,8 +0,0 @@
-# This shell script executes the Slurm jobs for training ConvNets with median summary as side-channel background information.
-
-sbatch 028_T-0600_unit01.sbatch
-sbatch 028_T-0600_unit02.sbatch
-sbatch 028_T-0600_unit03.sbatch
-sbatch 028_T-0600_unit05.sbatch
-sbatch 028_T-0600_unit07.sbatch
-sbatch 028_T-0600_unit10.sbatch
