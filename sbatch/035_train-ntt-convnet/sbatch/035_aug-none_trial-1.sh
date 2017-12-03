2a6455832947b6796cb6f94dc34bb1b04205b133
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-02 21:34:57 -0500

    upload 035 shell files

---
+++ sbatch/035_train-ntt-convnet/sbatch/035_aug-none_trial-1.sh
@@ -0,0 +1,12 @@
+# This shell script executes Slurm jobs for training
+# NTT convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 1.
+# Augmentation kind: none.
+
+sbatch 035_aug-none_unit01_trial-1.sbatch
+sbatch 035_aug-none_unit02_trial-1.sbatch
+sbatch 035_aug-none_unit03_trial-1.sbatch
+sbatch 035_aug-none_unit05_trial-1.sbatch
+sbatch 035_aug-none_unit07_trial-1.sbatch
+sbatch 035_aug-none_unit10_trial-1.sbatch
