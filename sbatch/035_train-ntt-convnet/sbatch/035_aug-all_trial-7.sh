ec39360cdcfdf6c14fd5f7c2029ca9fbe5535b77
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-06 01:45:19 -0500

    re-generate 035 shell files

---
+++ sbatch/035_train-ntt-convnet/sbatch/035_aug-all_trial-7.sh
@@ -0,0 +1,12 @@
+# This shell script executes Slurm jobs for training
+# NTT convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 7.
+# Augmentation kind: all.
+
+sbatch 035_aug-all_unit01_trial-7.sbatch
+sbatch 035_aug-all_unit02_trial-7.sbatch
+sbatch 035_aug-all_unit03_trial-7.sbatch
+sbatch 035_aug-all_unit05_trial-7.sbatch
+sbatch 035_aug-all_unit07_trial-7.sbatch
+sbatch 035_aug-all_unit10_trial-7.sbatch
