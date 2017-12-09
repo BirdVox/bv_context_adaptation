93f984001f06305bf78e3e43d914d0fbba319c33
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-09 11:44:54 -0500

    up

---
+++ sbatch/036_train-dot-convnet/sbatch/036_aug-all-but-noise_trial-2.sh
@@ -0,0 +1,12 @@
+# This shell script executes Slurm jobs for training
+# NTT convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 2.
+# Augmentation kind: all-but-noise.
+
+sbatch 036_aug-all-but-noise_unit01_trial-2.sbatch
+sbatch 036_aug-all-but-noise_unit02_trial-2.sbatch
+sbatch 036_aug-all-but-noise_unit03_trial-2.sbatch
+sbatch 036_aug-all-but-noise_unit05_trial-2.sbatch
+sbatch 036_aug-all-but-noise_unit07_trial-2.sbatch
+sbatch 036_aug-all-but-noise_unit10_trial-2.sbatch
---
+++ sbatch/036_train-dot-convnet/sbatch/036_aug-none_trial-2.sh
@@ -0,0 +1,12 @@
+# This shell script executes Slurm jobs for training
+# NTT convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 2.
+# Augmentation kind: none.
+
+sbatch 036_aug-none_unit01_trial-2.sbatch
+sbatch 036_aug-none_unit02_trial-2.sbatch
+sbatch 036_aug-none_unit03_trial-2.sbatch
+sbatch 036_aug-none_unit05_trial-2.sbatch
+sbatch 036_aug-none_unit07_trial-2.sbatch
+sbatch 036_aug-none_unit10_trial-2.sbatch
