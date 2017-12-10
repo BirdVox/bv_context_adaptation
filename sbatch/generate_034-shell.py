721b1768237f9fcf3ceb9bef90c99fe309f2ca88
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-10 18:44:31 -0500

    update generate_034-shell

--- sbatch/generate_034-shell.py
+++ sbatch/generate_034-shell.py
@@ -7,7 +7,7 @@ import localmodule
 
 
 # Define constants.
-aug_kinds = ["all", "noise", "none", "pitch", "stretch"]
+aug_kinds = ["all", "all-but-noise", "none"]
 units = localmodule.get_units()
 script_name = "034_evaluate-pcen-convnet-full-audio.py"
 script_path = os.path.join("..", "..", "..", "src", script_name)
