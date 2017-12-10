459c3a3c3ab067c9d526c3b176875585ac38f61d
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-10 15:45:41 -0500

    multiply = keras.layers.Dot(1, name="multiply")([spec_dense, bg_dense])

--- src/036_train-dot-convnet.py
+++ src/036_train-dot-convnet.py
@@ -277,7 +277,7 @@ bg_dense = keras.layers.Dense(64,
 
 # Combined channel.
 # Dot product
-multiply = keras.layers.Dot(0, name="multiply")([spec_dense, bg_dense])
+multiply = keras.layers.Dot(1, name="multiply")([spec_dense, bg_dense])
 
 
 # Parametric ReLU.
