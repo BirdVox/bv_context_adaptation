a4710188d6d37ce019a10c24b6a4baded056d753
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-11 02:09:13 -0500

    re-generate 034 shell files

--- sbatch/034_evaluate-pcen-convnet-full-audio/sbatch/034_aug-nonetest-unit02_predict-unit02_trial-0.sh
+++ sbatch/034_evaluate-pcen-convnet-full-audio/sbatch/034_aug-nonetest-unit02_predict-unit02_trial-0.sh
@@ -1,52 +1,31 @@
 # This shell script executes Slurm jobs for thresholding
 # Justin Salamon's ICASSP 2017 predictions of convolutional
 # neural network on BirdVox-70k full audio
-# with PCEN input.# Augmentation kind: none.
+# with PCEN input.
+# Augmentation kind: none.
 # Test unit: unit02.
 # Prediction unit: unit02.
 # Trial ID: 0.
 
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-000.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-010.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-020.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-030.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-040.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-050.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-060.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-070.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-080.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-090.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-100.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-110.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-120.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-130.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-140.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-150.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-160.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-170.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-180.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-190.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-200.sbatch
-
 sbatch 034_aug-none_test-unit02_predict-unit02_trial-0_th-210.sbatch
-
