e7a551990c9b7117d0e5ba7766c3a6a3015e8992
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-10 20:09:10 -0500

    bis

--- sbatch/generate_034-sbatch.py
+++ sbatch/generate_034-sbatch.py
@@ -37,56 +37,67 @@ for aug_kind_str in aug_kinds:
     # Loop over test units.
     for test_unit_str in units:
 
-        # Loop over trials.
-        for trial_id in range(n_trials):
+        # Retrieve fold such that unit_str is in the test set.
+        folds = localmodule.fold_units()
+        fold = [f for f in folds if test_unit_str in f[0]][0]
+        test_units = fold[0]
+        training_units = fold[1]
+        validation_units = fold[2]
+        predict_units = test_units + validation_units
 
-            # Loop over threshold groups:
-            for group_id in range(n_groups):
+        # Loop over prediction units.
+        for predict_unit_str in predict_units:
 
-                # Define threshold boundaries in group.
-                threshold_start = group_id * n_thresholds_per_group
-                threshold_range_str = str(threshold_start).zfill(3)
+            # Loop over trials.
+            for trial_id in range(n_trials):
 
-                # Define job name.
-                job_name = "_".join([
-                    script_name[:3],
-                    "aug-" + aug_kind_str,
-                    "test-" + test_unit_str,
-                    "trial-" + str(trial_id),
-                    "th-" + threshold_range_str
-                ])
+                # Loop over threshold groups:
+                for group_id in range(n_groups):
 
-                # Define file path.
-                file_name = job_name + ".sbatch"
-                file_path = os.path.join(sbatch_dir, file_name)
+                    # Define threshold boundaries in group.
+                    threshold_start = group_id * n_thresholds_per_group
+                    threshold_range_str = str(threshold_start).zfill(3)
 
-                # Define script path with arguments.
-                script_list = [
-                    script_path, aug_kind_str,
-                    test_unit_str, str(trial_id), threshold_range_str]
-                script_path_with_args = " ".join(script_list)
+                    # Define job name.
+                    job_name = "_".join([
+                        script_name[:3],
+                        "aug-" + aug_kind_str,
+                        "test-" + test_unit_str,
+                        "trial-" + str(trial_id),
+                        "th-" + threshold_range_str
+                    ])
 
-                # Define slurm path.
-                slurm_path = os.path.join("..", "slurm",
-                    "slurm_" + job_name + "_%j.out")
+                    # Define file path.
+                    file_name = job_name + ".sbatch"
+                    file_path = os.path.join(sbatch_dir, file_name)
 
-                # Write sbatch file.
-                with open(file_path, "w") as f:
-                    f.write("#!/bin/bash\n")
-                    f.write("\n")
-                    f.write("#BATCH --job-name=" + job_name + "\n")
-                    f.write("#SBATCH --nodes=1\n")
-                    f.write("#SBATCH --tasks-per-node=1\n")
-                    f.write("#SBATCH --cpus-per-task=1\n")
-                    f.write("#SBATCH --time=24:00:00\n")
-                    f.write("#SBATCH --mem=1GB\n")
-                    f.write("#SBATCH --output=" + slurm_path + "\n")
-                    f.write("\n")
-                    f.write("module purge\n")
-                    f.write("\n")
-                    f.write("# The first argument is the kind of data augmentation.\n")
-                    f.write("# The second argument is the test unit.")
-                    f.write("# The third argument is the prediction unit.")
-                    f.write("# The fourth argument is the trial index.")
-                    f.write("# The fifth argument is the threshold range.\n")
-                    f.write("python " + script_path_with_args)
+                    # Define script path with arguments.
+                    script_list = [
+                        script_path, aug_kind_str,
+                        test_unit_str, str(trial_id), threshold_range_str]
+                    script_path_with_args = " ".join(script_list)
+
+                    # Define slurm path.
+                    slurm_path = os.path.join("..", "slurm",
+                        "slurm_" + job_name + "_%j.out")
+
+                    # Write sbatch file.
+                    with open(file_path, "w") as f:
+                        f.write("#!/bin/bash\n")
+                        f.write("\n")
+                        f.write("#BATCH --job-name=" + job_name + "\n")
+                        f.write("#SBATCH --nodes=1\n")
+                        f.write("#SBATCH --tasks-per-node=1\n")
+                        f.write("#SBATCH --cpus-per-task=1\n")
+                        f.write("#SBATCH --time=24:00:00\n")
+                        f.write("#SBATCH --mem=1GB\n")
+                        f.write("#SBATCH --output=" + slurm_path + "\n")
+                        f.write("\n")
+                        f.write("module purge\n")
+                        f.write("\n")
+                        f.write("# The first argument is the kind of data augmentation.\n")
+                        f.write("# The second argument is the test unit.")
+                        f.write("# The third argument is the prediction unit.")
+                        f.write("# The fourth argument is the trial index.")
+                        f.write("# The fifth argument is the threshold range.\n")
+                        f.write("python " + script_path_with_args)
