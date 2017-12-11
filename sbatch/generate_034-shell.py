9a22ee34939d90f5b22b7f571e499077d821a833
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-10 20:08:07 -0500

    update generate_034-shell

--- sbatch/generate_034-shell.py
+++ sbatch/generate_034-shell.py
@@ -28,35 +28,43 @@ for aug_kind_str in aug_kinds:
     file_path = os.path.join(sbatch_dir,
         script_name[:3] + "_aug-" + aug_kind_str + ".sh")
 
-    # Open shell file
-    with open(file_path, "w") as f:
-        # Print header
-        f.write(
-            "# This shell script executes Slurm jobs for thresholding\n" +
-            "# Justin Salamon's ICASSP 2017 predictions of convolutional\n" +
-            "# neural network on " + dataset_name + " full audio.\n")
-        f.write("# Augmentation kind: " + aug_kind_str + ".\n")
-        f.write("\n")
+    # Loop over test units.
+    for test_unit_str in units:
 
         # Loop over test units.
-        for test_unit_str in units:
+        for predict_unit_str in units:
 
             # Loop over trials.
             for trial_id in range(n_trials):
 
-                # Define job name.
-                job_name = "_".join([
-                    script_name[:3],
-                    "aug-" + aug_kind_str,
-                    "test-" + test_unit_str,
-                    "trial-" + str(trial_id)])
-                sbatch_str = "sbatch " + job_name + ".sbatch"
-                f.write(sbatch_str + "\n")
-            f.write("\n")
+                # Open shell file
+                with open(file_path, "w") as f:
+                    # Print header
+                    f.write(
+                        "# This shell script executes Slurm jobs for thresholding\n" +
+                        "# Justin Salamon's ICASSP 2017 predictions of convolutional\n" +
+                        "# neural network on " + dataset_name + " full audio\n" +
+                        "# with PCEN input.")
+                    f.write("# Augmentation kind: " + aug_kind_str + ".\n")
+                    f.write("# Test unit: " + test_unit_str + ".\n")
+                    f.write("# Prediction unit: " + predict_unit_str + ".\n")
+                    f.write("# Trial ID: " + str(trial_id) + ".\n")
+                    f.write("\n")
+
+                    # Define job name.
+                    job_name = "_".join([
+                        script_name[:3],
+                        "aug-" + aug_kind_str,
+                        "test-" + test_unit_str,
+                        "predict-" + predict_unit_str,
+                        "trial-" + str(trial_id)])
+                    sbatch_str = "sbatch " + job_name + ".sbatch"
+                    f.write(sbatch_str + "\n")
+                    f.write("\n")
 
 
-    # Grant permission to execute the shell file.
-    # https://stackoverflow.com/a/30463972
-    mode = os.stat(file_path).st_mode
-    mode |= (mode & 0o444) >> 2
-    os.chmod(file_path, mode)
+                # Grant permission to execute the shell file.
+                # https://stackoverflow.com/a/30463972
+                mode = os.stat(file_path).st_mode
+                mode |= (mode & 0o444) >> 2
+                os.chmod(file_path, mode)
