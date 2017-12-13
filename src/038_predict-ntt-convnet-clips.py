9b7f839b270b63267c86ae21b4efb450bebf850e
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-12 19:52:38 -0500

    Open background container with h5py.

--- src/038_predict-ntt-convnet-clips.py
+++ src/038_predict-ntt-convnet-clips.py
@@ -77,6 +77,19 @@ lms_group = lms_container["logmelspec"]
 keys = list(lms_group.keys())
 
 
+# Open background container with h5py.
+bg_name = "_".join(
+    [dataset_name, "clip-logmelspec-backgrounds"])
+bg_dir = os.path.join(data_dir, bg_name)
+T_str = "T-" + str(bg_duration).zfill(4)
+T_dir = os.path.join(bg_dir, T_str)
+bg_name = "_".join(
+    [dataset_name, "background_summaries",
+     test_unit_str, T_str + ".hdf5"])
+bg_path = os.path.join(T_dir, bg_name)
+bg_container = h5py.File(bg_path, "r")
+
+
 # Create HDF5 container for predictions.
 clip_predictions_name = "_".join([
     dataset_name,
@@ -115,6 +128,8 @@ for key in keys:
 
     # Add trailing singleton dimension for Keras interoperability.
     X = X[np.newaxis, :, :, np.newaxis]
+
+
 
     # Predict.
     predicted_probability = model.predict(X)[0, 0]
