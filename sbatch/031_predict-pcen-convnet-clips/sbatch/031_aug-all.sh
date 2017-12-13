bfbf918bd85c29dcd9e3e062133748dcae6064d2
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-13 16:23:53 -0500

    write 031_aug-all.sh

---
+++ sbatch/031_predict-pcen-convnet-clips/sbatch/031_aug-all.sh
@@ -0,0 +1,10 @@
+./031_aug-all_trial-0.sh
+./031_aug-all_trial-1.sh
+./031_aug-all_trial-2.sh
+./031_aug-all_trial-3.sh
+./031_aug-all_trial-4.sh
+./031_aug-all_trial-5.sh
+./031_aug-all_trial-6.sh
+./031_aug-all_trial-7.sh
+./031_aug-all_trial-8.sh
+./031_aug-all_trial-8.sh
