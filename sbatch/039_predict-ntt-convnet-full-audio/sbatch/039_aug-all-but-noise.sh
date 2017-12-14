f26b1170f1f7fd9d5693ff649d7304e87fd8bc0c
Vincent <vincent.lostanlen@nyu.edu>
2017-12-14 14:51:10 -0500

    write 039_aug-all-but-noise.sh

---
+++ sbatch/039_predict-ntt-convnet-full-audio/sbatch/039_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./039_aug-all-but-noise_trial-0.sh
+./039_aug-all-but-noise_trial-1.sh
+./039_aug-all-but-noise_trial-2.sh
+./039_aug-all-but-noise_trial-3.sh
+./039_aug-all-but-noise_trial-4.sh
+./039_aug-all-but-noise_trial-5.sh
+./039_aug-all-but-noise_trial-6.sh
+./039_aug-all-but-noise_trial-7.sh
+./039_aug-all-but-noise_trial-8.sh
+./039_aug-all-but-noise_trial-9.sh
