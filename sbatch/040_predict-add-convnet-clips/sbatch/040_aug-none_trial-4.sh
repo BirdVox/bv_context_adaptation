7085538127f7e715df78788589f2df76d56c5569
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-14 00:22:55 -0500

    generate 040 sh

---
+++ sbatch/040_predict-add-convnet-clips/sbatch/040_aug-none_trial-4.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 4.
+# Augmentation kind: none.
+
+sbatch 040_aug-none_test-unit01_trial-4_predict-unit07.sbatch
+sbatch 040_aug-none_test-unit01_trial-4_predict-unit10.sbatch
+sbatch 040_aug-none_test-unit01_trial-4_predict-unit01.sbatch
+
+sbatch 040_aug-none_test-unit02_trial-4_predict-unit10.sbatch
+sbatch 040_aug-none_test-unit02_trial-4_predict-unit01.sbatch
+sbatch 040_aug-none_test-unit02_trial-4_predict-unit02.sbatch
+
+sbatch 040_aug-none_test-unit03_trial-4_predict-unit01.sbatch
+sbatch 040_aug-none_test-unit03_trial-4_predict-unit02.sbatch
+sbatch 040_aug-none_test-unit03_trial-4_predict-unit03.sbatch
+
+sbatch 040_aug-none_test-unit05_trial-4_predict-unit02.sbatch
+sbatch 040_aug-none_test-unit05_trial-4_predict-unit03.sbatch
+sbatch 040_aug-none_test-unit05_trial-4_predict-unit05.sbatch
+
+sbatch 040_aug-none_test-unit07_trial-4_predict-unit03.sbatch
+sbatch 040_aug-none_test-unit07_trial-4_predict-unit05.sbatch
+sbatch 040_aug-none_test-unit07_trial-4_predict-unit07.sbatch
+
+sbatch 040_aug-none_test-unit10_trial-4_predict-unit05.sbatch
+sbatch 040_aug-none_test-unit10_trial-4_predict-unit07.sbatch
+sbatch 040_aug-none_test-unit10_trial-4_predict-unit10.sbatch
+
