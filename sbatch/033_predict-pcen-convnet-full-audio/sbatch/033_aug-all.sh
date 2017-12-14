16721f3f8437626a5cf91c678d0a7bb43b3689db
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-14 18:37:36 -0500

    write 033_aug-all.sh

---
+++ sbatch/033_predict-pcen-convnet-full-audio/sbatch/033_aug-all.sh
@@ -0,0 +1,10 @@
+./033_aug-all_trial-0.sh
+./033_aug-all_trial-1.sh
+./033_aug-all_trial-2.sh
+./033_aug-all_trial-3.sh
+./033_aug-all_trial-4.sh
+./033_aug-all_trial-5.sh
+./033_aug-all_trial-6.sh
+./033_aug-all_trial-7.sh
+./033_aug-all_trial-8.sh
+./033_aug-all_trial-9.sh
