b75cd912bbacd75601a018d49c97ae0a07b26afd
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-20 11:17:59 -0500

    write 040_aug-all-but-noise.sh

---
+++ sbatch/040_predict-add-convnet-clips/sbatch/040_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./040_aug-all-but-noise_trial-0.sh
+./040_aug-all-but-noise_trial-1.sh
+./040_aug-all-but-noise_trial-2.sh
+./040_aug-all-but-noise_trial-3.sh
+./040_aug-all-but-noise_trial-4.sh
+./040_aug-all-but-noise_trial-5.sh
+./040_aug-all-but-noise_trial-6.sh
+./040_aug-all-but-noise_trial-7.sh
+./040_aug-all-but-noise_trial-8.sh
+./040_aug-all-but-noise_trial-9.sh
