8f96639bdc2e306193f17e05658a0be6f1e9833a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-20 11:16:52 -0500

    write 040_aug-all.sh

---
+++ sbatch/040_predict-add-convnet-clips/sbatch/040_aug-all.sh
@@ -0,0 +1,9 @@
+./040_aug-all_trial-1.sh
+./040_aug-all_trial-2.sh
+./040_aug-all_trial-3.sh
+./040_aug-all_trial-4.sh
+./040_aug-all_trial-5.sh
+./040_aug-all_trial-6.sh
+./040_aug-all_trial-7.sh
+./040_aug-all_trial-8.sh
+./040_aug-all_trial-9.sh
