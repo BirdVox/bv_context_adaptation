96c110c9285407a7a5fc8d0c2ba7107956d6ded1
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-20 11:18:21 -0500

    write 040_aug-none.sh

---
+++ sbatch/040_predict-add-convnet-clips/sbatch/040_aug-none.sh
@@ -0,0 +1,10 @@
+./040_aug-none_trial-0.sh
+./040_aug-none_trial-1.sh
+./040_aug-none_trial-2.sh
+./040_aug-none_trial-3.sh
+./040_aug-none_trial-4.sh
+./040_aug-none_trial-5.sh
+./040_aug-none_trial-6.sh
+./040_aug-none_trial-7.sh
+./040_aug-none_trial-8.sh
+./040_aug-none_trial-9.sh
