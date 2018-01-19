8e1c02f9c9e794854e1bc190dda091b84018cb04
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 14:00:46 -0500

    write 038 shell files

---
+++ sbatch/038_predict-ntt-convnet-clips/sbatch/038_aug-none.sh
@@ -0,0 +1,10 @@
+./038_aug-none_trial-0.sh
+./038_aug-none_trial-1.sh
+./038_aug-none_trial-2.sh
+./038_aug-none_trial-3.sh
+./038_aug-none_trial-4.sh
+./038_aug-none_trial-5.sh
+./038_aug-none_trial-6.sh
+./038_aug-none_trial-7.sh
+./038_aug-none_trial-8.sh
+./038_aug-none_trial-9.sh
