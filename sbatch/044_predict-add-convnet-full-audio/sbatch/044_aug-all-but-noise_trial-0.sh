cf2c118ced13427822b7a8bc4ab7aea28fbe2143
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 17:11:06 -0500

    write generate_044-sbatch and -shell, generate sbatch and shell files

---
+++ sbatch/044_predict-add-convnet-full-audio/sbatch/044_aug-all-but-noise_trial-0.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with adaptive threshold convolutional neural network
+# on BirdVox-70k, full audio.
+# Trial ID: 0.
+# Augmentation kind: all-but-noise.
+
+sbatch 044_aug-all-but-noise_test-unit01_trial-0_predict-unit07.sbatch
+sbatch 044_aug-all-but-noise_test-unit01_trial-0_predict-unit10.sbatch
+sbatch 044_aug-all-but-noise_test-unit01_trial-0_predict-unit01.sbatch
+
+sbatch 044_aug-all-but-noise_test-unit02_trial-0_predict-unit10.sbatch
+sbatch 044_aug-all-but-noise_test-unit02_trial-0_predict-unit01.sbatch
+sbatch 044_aug-all-but-noise_test-unit02_trial-0_predict-unit02.sbatch
+
+sbatch 044_aug-all-but-noise_test-unit03_trial-0_predict-unit01.sbatch
+sbatch 044_aug-all-but-noise_test-unit03_trial-0_predict-unit02.sbatch
+sbatch 044_aug-all-but-noise_test-unit03_trial-0_predict-unit03.sbatch
+
+sbatch 044_aug-all-but-noise_test-unit05_trial-0_predict-unit02.sbatch
+sbatch 044_aug-all-but-noise_test-unit05_trial-0_predict-unit03.sbatch
+sbatch 044_aug-all-but-noise_test-unit05_trial-0_predict-unit05.sbatch
+
+sbatch 044_aug-all-but-noise_test-unit07_trial-0_predict-unit03.sbatch
+sbatch 044_aug-all-but-noise_test-unit07_trial-0_predict-unit05.sbatch
+sbatch 044_aug-all-but-noise_test-unit07_trial-0_predict-unit07.sbatch
+
+sbatch 044_aug-all-but-noise_test-unit10_trial-0_predict-unit05.sbatch
+sbatch 044_aug-all-but-noise_test-unit10_trial-0_predict-unit07.sbatch
+sbatch 044_aug-all-but-noise_test-unit10_trial-0_predict-unit10.sbatch
+
---
+++ sbatch/044_predict-add-convnet-full-audio/sbatch/044_aug-all_trial-4.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with adaptive threshold convolutional neural network
+# on BirdVox-70k, full audio.
+# Trial ID: 4.
+# Augmentation kind: all.
+
+sbatch 044_aug-all_test-unit01_trial-4_predict-unit07.sbatch
+sbatch 044_aug-all_test-unit01_trial-4_predict-unit10.sbatch
+sbatch 044_aug-all_test-unit01_trial-4_predict-unit01.sbatch
+
+sbatch 044_aug-all_test-unit02_trial-4_predict-unit10.sbatch
+sbatch 044_aug-all_test-unit02_trial-4_predict-unit01.sbatch
+sbatch 044_aug-all_test-unit02_trial-4_predict-unit02.sbatch
+
+sbatch 044_aug-all_test-unit03_trial-4_predict-unit01.sbatch
+sbatch 044_aug-all_test-unit03_trial-4_predict-unit02.sbatch
+sbatch 044_aug-all_test-unit03_trial-4_predict-unit03.sbatch
+
+sbatch 044_aug-all_test-unit05_trial-4_predict-unit02.sbatch
+sbatch 044_aug-all_test-unit05_trial-4_predict-unit03.sbatch
+sbatch 044_aug-all_test-unit05_trial-4_predict-unit05.sbatch
+
+sbatch 044_aug-all_test-unit07_trial-4_predict-unit03.sbatch
+sbatch 044_aug-all_test-unit07_trial-4_predict-unit05.sbatch
+sbatch 044_aug-all_test-unit07_trial-4_predict-unit07.sbatch
+
+sbatch 044_aug-all_test-unit10_trial-4_predict-unit05.sbatch
+sbatch 044_aug-all_test-unit10_trial-4_predict-unit07.sbatch
+sbatch 044_aug-all_test-unit10_trial-4_predict-unit10.sbatch
+
