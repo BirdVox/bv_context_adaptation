15101f8a3ab21fa74aeb3e48c641809ac0bd7e85
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 13:14:54 -0500

    write 037 shell files

---
+++ sbatch/037_train-add-convnet/slurm/037_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./037_aug-all-but-noise_trial-0.sh
+./037_aug-all-but-noise_trial-1.sh
+./037_aug-all-but-noise_trial-2.sh
+./037_aug-all-but-noise_trial-3.sh
+./037_aug-all-but-noise_trial-4.sh
+./037_aug-all-but-noise_trial-5.sh
+./037_aug-all-but-noise_trial-6.sh
+./037_aug-all-but-noise_trial-7.sh
+./037_aug-all-but-noise_trial-8.sh
+./037_aug-all-but-noise_trial-9.sh
