2ecde045fb7bd38178a0b9fb904cf2cb87cdbd31
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 16:58:05 -0500

    043 shell files

---
+++ sbatch/043_train-pcen-ntt-convnet/sbatch/043_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./043_aug-all-but-noise_trial-0.sh
+./043_aug-all-but-noise_trial-1.sh
+./043_aug-all-but-noise_trial-2.sh
+./043_aug-all-but-noise_trial-3.sh
+./043_aug-all-but-noise_trial-4.sh
+./043_aug-all-but-noise_trial-5.sh
+./043_aug-all-but-noise_trial-6.sh
+./043_aug-all-but-noise_trial-7.sh
+./043_aug-all-but-noise_trial-8.sh
+./043_aug-all-but-noise_trial-9.sh
