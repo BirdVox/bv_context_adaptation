ca659b5f544c7f07b1ee76bf7c918b82a51c10a3
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 17:35:59 -0500

    write 046 shell files that call shell files themselves

---
+++ sbatch/046_train-pcen-add-convnet/sbatch/046_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./046_aug-all-but-noise_trial-0.sh
+./046_aug-all-but-noise_trial-1.sh
+./046_aug-all-but-noise_trial-2.sh
+./046_aug-all-but-noise_trial-3.sh
+./046_aug-all-but-noise_trial-4.sh
+./046_aug-all-but-noise_trial-5.sh
+./046_aug-all-but-noise_trial-6.sh
+./046_aug-all-but-noise_trial-7.sh
+./046_aug-all-but-noise_trial-8.sh
+./046_aug-all-but-noise_trial-9.sh
---
+++ sbatch/046_train-pcen-add-convnet/sbatch/046_aug-none.sh
@@ -0,0 +1,10 @@
+./046_aug-none_trial-0.sh
+./046_aug-none_trial-1.sh
+./046_aug-none_trial-2.sh
+./046_aug-none_trial-3.sh
+./046_aug-none_trial-4.sh
+./046_aug-none_trial-5.sh
+./046_aug-none_trial-6.sh
+./046_aug-none_trial-7.sh
+./046_aug-none_trial-8.sh
+./046_aug-none_trial-9.sh
