f584d92de7376583e6fcd68a351bf6cd938d4cd6
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 16:24:33 -0500

    write 030 shell files

---
+++ sbatch/030_train-pcen-convnet/sbatch/030_aug-all-but-noise.sh
@@ -0,0 +1,10 @@
+./030_aug-all-but-noise_trial-0.sh
+./030_aug-all-but-noise_trial-1.sh
+./030_aug-all-but-noise_trial-2.sh
+./030_aug-all-but-noise_trial-3.sh
+./030_aug-all-but-noise_trial-4.sh
+./030_aug-all-but-noise_trial-5.sh
+./030_aug-all-but-noise_trial-6.sh
+./030_aug-all-but-noise_trial-7.sh
+./030_aug-all-but-noise_trial-8.sh
+./030_aug-all-but-noise_trial-9.sh
