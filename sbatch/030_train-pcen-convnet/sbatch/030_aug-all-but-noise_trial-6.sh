be4e8e6d500aecc099a41de9e70a242b43061d96
Vincent <vincent.lostanlen@nyu.edu>
2018-01-19 16:20:57 -0500

    up

---
+++ sbatch/030_train-pcen-convnet/sbatch/030_aug-all-but-noise_trial-6.sh
@@ -0,0 +1,13 @@
+# This shell script executes Slurm jobs for training
+# Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k with PCEN input instead of
+# log-mel-spectrogram (logmelspec) input.
+# Trial ID: 6.
+# Augmentation kind: all-but-noise.
+
+sbatch 030_aug-all-but-noise_unit01_trial-6.sbatch
+sbatch 030_aug-all-but-noise_unit02_trial-6.sbatch
+sbatch 030_aug-all-but-noise_unit03_trial-6.sbatch
+sbatch 030_aug-all-but-noise_unit05_trial-6.sbatch
+sbatch 030_aug-all-but-noise_unit07_trial-6.sbatch
+sbatch 030_aug-all-but-noise_unit10_trial-6.sbatch
