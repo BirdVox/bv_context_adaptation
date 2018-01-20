7e00865cd373d3e3aad0ab00eebc7718371fc130
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-19 22:33:45 -0500

    generate 047 sbatch and shell files

---
+++ sbatch/047_predict-pcen-ntt-convnet-clips/sbatch/047_aug-all_trial-5.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with NTT-like convolutional neural network
+# on BirdVox-70k with PCEN input.
+# Trial ID: 5.
+# Augmentation kind: all.
+
+sbatch 047_aug-all_test-unit01_trial-5_predict-unit07.sbatch
+sbatch 047_aug-all_test-unit01_trial-5_predict-unit10.sbatch
+sbatch 047_aug-all_test-unit01_trial-5_predict-unit01.sbatch
+
+sbatch 047_aug-all_test-unit02_trial-5_predict-unit10.sbatch
+sbatch 047_aug-all_test-unit02_trial-5_predict-unit01.sbatch
+sbatch 047_aug-all_test-unit02_trial-5_predict-unit02.sbatch
+
+sbatch 047_aug-all_test-unit03_trial-5_predict-unit01.sbatch
+sbatch 047_aug-all_test-unit03_trial-5_predict-unit02.sbatch
+sbatch 047_aug-all_test-unit03_trial-5_predict-unit03.sbatch
+
+sbatch 047_aug-all_test-unit05_trial-5_predict-unit02.sbatch
+sbatch 047_aug-all_test-unit05_trial-5_predict-unit03.sbatch
+sbatch 047_aug-all_test-unit05_trial-5_predict-unit05.sbatch
+
+sbatch 047_aug-all_test-unit07_trial-5_predict-unit03.sbatch
+sbatch 047_aug-all_test-unit07_trial-5_predict-unit05.sbatch
+sbatch 047_aug-all_test-unit07_trial-5_predict-unit07.sbatch
+
+sbatch 047_aug-all_test-unit10_trial-5_predict-unit05.sbatch
+sbatch 047_aug-all_test-unit10_trial-5_predict-unit07.sbatch
+sbatch 047_aug-all_test-unit10_trial-5_predict-unit10.sbatch
+
