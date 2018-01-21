4acd53cf7903f34a5a3e2723f85e7a54fc1e50da
Vincent <vincent.lostanlen@nyu.edu>
2018-01-21 14:39:51 -0500

    up

---
+++ sbatch/031_predict-pcen-convnet-clips/sbatch/031_aug-all-but-noise_trial-1.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with Justin Salamon's ICASSP 2017 convolutional neural network
+# on BirdVox-70k.
+# Trial ID: 1.
+# Augmentation kind: all-but-noise.
+
+sbatch 031_aug-all-but-noise_test-unit01_trial-1_predict-unit07.sbatch
+sbatch 031_aug-all-but-noise_test-unit01_trial-1_predict-unit10.sbatch
+sbatch 031_aug-all-but-noise_test-unit01_trial-1_predict-unit01.sbatch
+
+sbatch 031_aug-all-but-noise_test-unit02_trial-1_predict-unit10.sbatch
+sbatch 031_aug-all-but-noise_test-unit02_trial-1_predict-unit01.sbatch
+sbatch 031_aug-all-but-noise_test-unit02_trial-1_predict-unit02.sbatch
+
+sbatch 031_aug-all-but-noise_test-unit03_trial-1_predict-unit01.sbatch
+sbatch 031_aug-all-but-noise_test-unit03_trial-1_predict-unit02.sbatch
+sbatch 031_aug-all-but-noise_test-unit03_trial-1_predict-unit03.sbatch
+
+sbatch 031_aug-all-but-noise_test-unit05_trial-1_predict-unit02.sbatch
+sbatch 031_aug-all-but-noise_test-unit05_trial-1_predict-unit03.sbatch
+sbatch 031_aug-all-but-noise_test-unit05_trial-1_predict-unit05.sbatch
+
+sbatch 031_aug-all-but-noise_test-unit07_trial-1_predict-unit03.sbatch
+sbatch 031_aug-all-but-noise_test-unit07_trial-1_predict-unit05.sbatch
+sbatch 031_aug-all-but-noise_test-unit07_trial-1_predict-unit07.sbatch
+
+sbatch 031_aug-all-but-noise_test-unit10_trial-1_predict-unit05.sbatch
+sbatch 031_aug-all-but-noise_test-unit10_trial-1_predict-unit07.sbatch
+sbatch 031_aug-all-but-noise_test-unit10_trial-1_predict-unit10.sbatch
+
