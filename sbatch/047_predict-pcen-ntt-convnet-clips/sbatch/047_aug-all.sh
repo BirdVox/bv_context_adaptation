85e25f436edb18cb6742fa3b29be91a27e3ffffb
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-23 19:42:19 -0500

    write 047 meta-shell files

---
+++ sbatch/047_predict-pcen-ntt-convnet-clips/sbatch/047_aug-all.sh
@@ -0,0 +1,10 @@
+./047_aug-all_trial-0.sh
+./047_aug-all_trial-1.sh
+./047_aug-all_trial-2.sh
+./047_aug-all_trial-3.sh
+./047_aug-all_trial-4.sh
+./047_aug-all_trial-5.sh
+./047_aug-all_trial-6.sh
+./047_aug-all_trial-7.sh
+./047_aug-all_trial-8.sh
+./047_aug-all_trial-9.sh
