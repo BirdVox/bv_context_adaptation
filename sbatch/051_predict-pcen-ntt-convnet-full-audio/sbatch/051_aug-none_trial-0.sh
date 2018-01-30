ec64cefffd5b8ad2f5c2212ebe76961d71582e1c
Vincent <vincent.lostanlen@nyu.edu>
2018-01-30 16:07:39 -0500

    up

---
+++ sbatch/051_predict-pcen-ntt-convnet-full-audio/sbatch/051_aug-none_trial-0.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with NTT-like convolutional neural network
+# on BirdVox-70k, full audio, with PCEN input.
+# Trial ID: 0.
+# Augmentation kind: none.
+
+sbatch 051_aug-none_test-unit01_trial-0_predict-unit07.sbatch
+sbatch 051_aug-none_test-unit01_trial-0_predict-unit10.sbatch
+sbatch 051_aug-none_test-unit01_trial-0_predict-unit01.sbatch
+
+sbatch 051_aug-none_test-unit02_trial-0_predict-unit10.sbatch
+sbatch 051_aug-none_test-unit02_trial-0_predict-unit01.sbatch
+sbatch 051_aug-none_test-unit02_trial-0_predict-unit02.sbatch
+
+sbatch 051_aug-none_test-unit03_trial-0_predict-unit01.sbatch
+sbatch 051_aug-none_test-unit03_trial-0_predict-unit02.sbatch
+sbatch 051_aug-none_test-unit03_trial-0_predict-unit03.sbatch
+
+sbatch 051_aug-none_test-unit05_trial-0_predict-unit02.sbatch
+sbatch 051_aug-none_test-unit05_trial-0_predict-unit03.sbatch
+sbatch 051_aug-none_test-unit05_trial-0_predict-unit05.sbatch
+
+sbatch 051_aug-none_test-unit07_trial-0_predict-unit03.sbatch
+sbatch 051_aug-none_test-unit07_trial-0_predict-unit05.sbatch
+sbatch 051_aug-none_test-unit07_trial-0_predict-unit07.sbatch
+
+sbatch 051_aug-none_test-unit10_trial-0_predict-unit05.sbatch
+sbatch 051_aug-none_test-unit10_trial-0_predict-unit07.sbatch
+sbatch 051_aug-none_test-unit10_trial-0_predict-unit10.sbatch
+
