ef1528aad356bf49352ece24e99a4aa053cc20c0
Vincent <vincent.lostanlen@nyu.edu>
2018-01-30 15:32:46 -0500

    generate 050 sbatch and shell files

---
+++ sbatch/050_predict-pcen-add-convnet-full-audio/sbatch/050_aug-all-but-noise_trial-1.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with adaptive threshold convolutional neural network
+# on BirdVox-70k, full audio, with PCEN input.
+# Trial ID: 1.
+# Augmentation kind: all-but-noise.
+
+sbatch 050_aug-all-but-noise_test-unit01_trial-1_predict-unit07.sbatch
+sbatch 050_aug-all-but-noise_test-unit01_trial-1_predict-unit10.sbatch
+sbatch 050_aug-all-but-noise_test-unit01_trial-1_predict-unit01.sbatch
+
+sbatch 050_aug-all-but-noise_test-unit02_trial-1_predict-unit10.sbatch
+sbatch 050_aug-all-but-noise_test-unit02_trial-1_predict-unit01.sbatch
+sbatch 050_aug-all-but-noise_test-unit02_trial-1_predict-unit02.sbatch
+
+sbatch 050_aug-all-but-noise_test-unit03_trial-1_predict-unit01.sbatch
+sbatch 050_aug-all-but-noise_test-unit03_trial-1_predict-unit02.sbatch
+sbatch 050_aug-all-but-noise_test-unit03_trial-1_predict-unit03.sbatch
+
+sbatch 050_aug-all-but-noise_test-unit05_trial-1_predict-unit02.sbatch
+sbatch 050_aug-all-but-noise_test-unit05_trial-1_predict-unit03.sbatch
+sbatch 050_aug-all-but-noise_test-unit05_trial-1_predict-unit05.sbatch
+
+sbatch 050_aug-all-but-noise_test-unit07_trial-1_predict-unit03.sbatch
+sbatch 050_aug-all-but-noise_test-unit07_trial-1_predict-unit05.sbatch
+sbatch 050_aug-all-but-noise_test-unit07_trial-1_predict-unit07.sbatch
+
+sbatch 050_aug-all-but-noise_test-unit10_trial-1_predict-unit05.sbatch
+sbatch 050_aug-all-but-noise_test-unit10_trial-1_predict-unit07.sbatch
+sbatch 050_aug-all-but-noise_test-unit10_trial-1_predict-unit10.sbatch
+
