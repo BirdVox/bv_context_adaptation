61512eb4e3e88698b800c1d98bff16b40953bb3b
Vincent <vincent.lostanlen@nyu.edu>
2018-01-31 15:45:08 -0500

    up

---
+++ sbatch/053.sh
@@ -0,0 +1,9 @@
+# This shell script executes the Slurm jobs for computing spectral flux on continuous audio.
+
+sbatch 053_unit01.sbatch
+sbatch 053_unit02.sbatch
+sbatch 053_unit03.sbatch
+sbatch 053_unit05.sbatch
+sbatch 053_unit07.sbatch
+sbatch 053_unit10.sbatch
+
