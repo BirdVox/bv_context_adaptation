cbe81d4144babbc3cbedbfedae125b230d47e09d
Vincent <vincent.lostanlen@nyu.edu>
2018-02-19 21:36:17 -0500

    up

---
+++ sbatch/041b_compute-full-background-summaries/sbatch/041b_T-0001.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing backgrounds on clips.
+
+sbatch 041b_T-0001_unit01.sbatch
+sbatch 041b_T-0001_unit02.sbatch
+sbatch 041b_T-0001_unit03.sbatch
+sbatch 041b_T-0001_unit05.sbatch
+sbatch 041b_T-0001_unit07.sbatch
+sbatch 041b_T-0001_unit10.sbatch
---
+++ sbatch/041b_compute-full-background-summaries/sbatch/041b_T-7200.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing backgrounds on clips.
+
+sbatch 041b_T-7200_unit01.sbatch
+sbatch 041b_T-7200_unit02.sbatch
+sbatch 041b_T-7200_unit03.sbatch
+sbatch 041b_T-7200_unit05.sbatch
+sbatch 041b_T-7200_unit07.sbatch
+sbatch 041b_T-7200_unit10.sbatch
