62ea2dadb248c484867b4026af8d4c1ddf6d765d
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-02-21 01:50:41 -0500

    write 045 meta-shell files

---
+++ sbatch/045_evaluate-add-convnet-full-audio/sbatch/045_aug-all.sh
@@ -0,0 +1,254 @@
+# This shell script executes Slurm jobs for thresholding
+# Justin Salamon's ICASSP 2017 predictions of convolutional
+# neural network on BirdVox-70k full audio
+# with PCEN input.
+# Augmentation kind: all.
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-0.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-0.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-0.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-0.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-0.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-0.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-0.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-0.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-0.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-0.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-0.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-0.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-0.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-0.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-0.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-0.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-0.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-0.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-1.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-1.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-1.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-1.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-1.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-1.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-1.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-1.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-1.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-1.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-1.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-1.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-1.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-1.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-1.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-1.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-1.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-1.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-2.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-2.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-2.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-2.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-2.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-2.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-2.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-2.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-2.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-2.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-2.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-2.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-2.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-2.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-2.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-2.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-2.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-2.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-3.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-3.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-3.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-3.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-3.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-3.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-3.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-3.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-3.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-3.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-3.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-3.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-3.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-3.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-3.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-3.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-3.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-3.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-4.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-4.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-4.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-4.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-4.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-4.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-4.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-4.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-4.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-4.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-4.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-4.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-4.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-4.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-4.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-4.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-4.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-4.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-5.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-5.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-5.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-5.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-5.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-5.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-5.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-5.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-5.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-5.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-5.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-5.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-5.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-5.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-5.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-5.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-5.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-5.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-6.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-6.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-6.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-6.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-6.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-6.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-6.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-6.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-6.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-6.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-6.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-6.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-6.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-6.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-6.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-6.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-6.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-6.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-7.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-7.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-7.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-7.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-7.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-7.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-7.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-7.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-7.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-7.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-7.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-7.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-7.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-7.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-7.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-7.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-7.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-7.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-8.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-8.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-8.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-8.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-8.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-8.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-8.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-8.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-8.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-8.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-8.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-8.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-8.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-8.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-8.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-8.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-8.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-8.sbatch
+
+
+sbatch 042_aug-all_test-unit01_predict-unit01_trial-9.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit07_trial-9.sbatch
+sbatch 042_aug-all_test-unit01_predict-unit10_trial-9.sbatch
+
+sbatch 042_aug-all_test-unit02_predict-unit02_trial-9.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit10_trial-9.sbatch
+sbatch 042_aug-all_test-unit02_predict-unit01_trial-9.sbatch
+
+sbatch 042_aug-all_test-unit03_predict-unit03_trial-9.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit01_trial-9.sbatch
+sbatch 042_aug-all_test-unit03_predict-unit02_trial-9.sbatch
+
+sbatch 042_aug-all_test-unit05_predict-unit05_trial-9.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit02_trial-9.sbatch
+sbatch 042_aug-all_test-unit05_predict-unit03_trial-9.sbatch
+
+sbatch 042_aug-all_test-unit07_predict-unit07_trial-9.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit03_trial-9.sbatch
+sbatch 042_aug-all_test-unit07_predict-unit05_trial-9.sbatch
+
+sbatch 042_aug-all_test-unit10_predict-unit10_trial-9.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit05_trial-9.sbatch
+sbatch 042_aug-all_test-unit10_predict-unit07_trial-9.sbatch
