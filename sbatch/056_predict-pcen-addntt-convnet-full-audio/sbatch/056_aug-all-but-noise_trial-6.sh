30ba44b3d6e7ab1a8259d9973c3536c682adf25a
Vincent <vincent.lostanlen@nyu.edu>
2018-02-25 13:03:21 -0500

    up

---
+++ sbatch/056_predict-pcen-addntt-convnet-full-audio/sbatch/056_aug-all-but-noise_trial-6.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with adaptive threshold, mixture-of-experts convolutional neural network
+# on BirdVox-70k, full audio, with PCEN input.
+# Trial ID: 6.
+# Augmentation kind: all-but-noise.
+
+sbatch 056_aug-all-but-noise_test-unit01_trial-6_predict-unit07.sbatch
+sbatch 056_aug-all-but-noise_test-unit01_trial-6_predict-unit10.sbatch
+sbatch 056_aug-all-but-noise_test-unit01_trial-6_predict-unit01.sbatch
+
+sbatch 056_aug-all-but-noise_test-unit02_trial-6_predict-unit10.sbatch
+sbatch 056_aug-all-but-noise_test-unit02_trial-6_predict-unit01.sbatch
+sbatch 056_aug-all-but-noise_test-unit02_trial-6_predict-unit02.sbatch
+
+sbatch 056_aug-all-but-noise_test-unit03_trial-6_predict-unit01.sbatch
+sbatch 056_aug-all-but-noise_test-unit03_trial-6_predict-unit02.sbatch
+sbatch 056_aug-all-but-noise_test-unit03_trial-6_predict-unit03.sbatch
+
+sbatch 056_aug-all-but-noise_test-unit05_trial-6_predict-unit02.sbatch
+sbatch 056_aug-all-but-noise_test-unit05_trial-6_predict-unit03.sbatch
+sbatch 056_aug-all-but-noise_test-unit05_trial-6_predict-unit05.sbatch
+
+sbatch 056_aug-all-but-noise_test-unit07_trial-6_predict-unit03.sbatch
+sbatch 056_aug-all-but-noise_test-unit07_trial-6_predict-unit05.sbatch
+sbatch 056_aug-all-but-noise_test-unit07_trial-6_predict-unit07.sbatch
+
+sbatch 056_aug-all-but-noise_test-unit10_trial-6_predict-unit05.sbatch
+sbatch 056_aug-all-but-noise_test-unit10_trial-6_predict-unit07.sbatch
+sbatch 056_aug-all-but-noise_test-unit10_trial-6_predict-unit10.sbatch
+
---
+++ sbatch/056_predict-pcen-addntt-convnet-full-audio/sbatch/056_aug-none_trial-9.sh
@@ -0,0 +1,30 @@
+# This shell script executes Slurm jobs for running predictions
+# with adaptive threshold, mixture-of-experts convolutional neural network
+# on BirdVox-70k, full audio, with PCEN input.
+# Trial ID: 9.
+# Augmentation kind: none.
+
+sbatch 056_aug-none_test-unit01_trial-9_predict-unit07.sbatch
+sbatch 056_aug-none_test-unit01_trial-9_predict-unit10.sbatch
+sbatch 056_aug-none_test-unit01_trial-9_predict-unit01.sbatch
+
+sbatch 056_aug-none_test-unit02_trial-9_predict-unit10.sbatch
+sbatch 056_aug-none_test-unit02_trial-9_predict-unit01.sbatch
+sbatch 056_aug-none_test-unit02_trial-9_predict-unit02.sbatch
+
+sbatch 056_aug-none_test-unit03_trial-9_predict-unit01.sbatch
+sbatch 056_aug-none_test-unit03_trial-9_predict-unit02.sbatch
+sbatch 056_aug-none_test-unit03_trial-9_predict-unit03.sbatch
+
+sbatch 056_aug-none_test-unit05_trial-9_predict-unit02.sbatch
+sbatch 056_aug-none_test-unit05_trial-9_predict-unit03.sbatch
+sbatch 056_aug-none_test-unit05_trial-9_predict-unit05.sbatch
+
+sbatch 056_aug-none_test-unit07_trial-9_predict-unit03.sbatch
+sbatch 056_aug-none_test-unit07_trial-9_predict-unit05.sbatch
+sbatch 056_aug-none_test-unit07_trial-9_predict-unit07.sbatch
+
+sbatch 056_aug-none_test-unit10_trial-9_predict-unit05.sbatch
+sbatch 056_aug-none_test-unit10_trial-9_predict-unit07.sbatch
+sbatch 056_aug-none_test-unit10_trial-9_predict-unit10.sbatch
+
