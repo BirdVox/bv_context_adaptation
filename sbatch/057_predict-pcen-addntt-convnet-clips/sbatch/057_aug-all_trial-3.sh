a60e73cca9fe0616208b38c0fba3bce0b2ef722c
Vincent <vincent.lostanlen@nyu.edu>
2018-02-26 18:27:21 -0500

    up

---
+++ sbatch/057_predict-pcen-addntt-convnet-clips/sbatch/057_aug-all_trial-3.sh
@@ -0,0 +1,31 @@
+# This shell script executes Slurm jobs for running predictions
+# with convolutional neural network with
+# adaptive threshold and mixture of experts side channel
+# on BirdVox-70k with PCEN input.
+# Trial ID: 3.
+# Augmentation kind: all.
+
+sbatch 057_aug-all_test-unit01_trial-3_predict-unit07.sbatch
+sbatch 057_aug-all_test-unit01_trial-3_predict-unit10.sbatch
+sbatch 057_aug-all_test-unit01_trial-3_predict-unit01.sbatch
+
+sbatch 057_aug-all_test-unit02_trial-3_predict-unit10.sbatch
+sbatch 057_aug-all_test-unit02_trial-3_predict-unit01.sbatch
+sbatch 057_aug-all_test-unit02_trial-3_predict-unit02.sbatch
+
+sbatch 057_aug-all_test-unit03_trial-3_predict-unit01.sbatch
+sbatch 057_aug-all_test-unit03_trial-3_predict-unit02.sbatch
+sbatch 057_aug-all_test-unit03_trial-3_predict-unit03.sbatch
+
+sbatch 057_aug-all_test-unit05_trial-3_predict-unit02.sbatch
+sbatch 057_aug-all_test-unit05_trial-3_predict-unit03.sbatch
+sbatch 057_aug-all_test-unit05_trial-3_predict-unit05.sbatch
+
+sbatch 057_aug-all_test-unit07_trial-3_predict-unit03.sbatch
+sbatch 057_aug-all_test-unit07_trial-3_predict-unit05.sbatch
+sbatch 057_aug-all_test-unit07_trial-3_predict-unit07.sbatch
+
+sbatch 057_aug-all_test-unit10_trial-3_predict-unit05.sbatch
+sbatch 057_aug-all_test-unit10_trial-3_predict-unit07.sbatch
+sbatch 057_aug-all_test-unit10_trial-3_predict-unit10.sbatch
+
