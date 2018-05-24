83fbc93b07241d8499e677382c164887c711b4ee
Vincent <vincent.lostanlen@nyu.edu>
2018-05-23 20:39:52 -0400

    up

---
+++ sbatch/060_compute-full-birdpcen/sbatch/060.sh
@@ -0,0 +1,9 @@
+# This shell script executes the Slurm jobs for computing PCENs (librosa) on continuous audio.
+
+sbatch 060_unit01.sbatch
+sbatch 060_unit02.sbatch
+sbatch 060_unit03.sbatch
+sbatch 060_unit05.sbatch
+sbatch 060_unit07.sbatch
+sbatch 060_unit10.sbatch
+
