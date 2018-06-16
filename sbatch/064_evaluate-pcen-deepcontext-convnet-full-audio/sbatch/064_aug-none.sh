4077d9b7e5055f71454672d45bfc7ec4fa31d9a8
Vincent <vincent.lostanlen@nyu.edu>
2018-06-16 09:06:17 -0400

    up

---
+++ sbatch/064_evaluate-pcen-deepcontext-convnet-full-audio/sbatch/064_aug-none.sh
@@ -0,0 +1,71 @@
+# This shell script executes Slurm jobs for evaluating
+# deep context convolutional neural network
+# on BirdVox-70k full audio with PCEN input.
+# Augmentation kind: none.
+
+sbatch 064_aug-none_test-unit01_trial-0.sbatch
+sbatch 064_aug-none_test-unit01_trial-1.sbatch
+sbatch 064_aug-none_test-unit01_trial-2.sbatch
+sbatch 064_aug-none_test-unit01_trial-3.sbatch
+sbatch 064_aug-none_test-unit01_trial-4.sbatch
+sbatch 064_aug-none_test-unit01_trial-5.sbatch
+sbatch 064_aug-none_test-unit01_trial-6.sbatch
+sbatch 064_aug-none_test-unit01_trial-7.sbatch
+sbatch 064_aug-none_test-unit01_trial-8.sbatch
+sbatch 064_aug-none_test-unit01_trial-9.sbatch
+
+sbatch 064_aug-none_test-unit02_trial-0.sbatch
+sbatch 064_aug-none_test-unit02_trial-1.sbatch
+sbatch 064_aug-none_test-unit02_trial-2.sbatch
+sbatch 064_aug-none_test-unit02_trial-3.sbatch
+sbatch 064_aug-none_test-unit02_trial-4.sbatch
+sbatch 064_aug-none_test-unit02_trial-5.sbatch
+sbatch 064_aug-none_test-unit02_trial-6.sbatch
+sbatch 064_aug-none_test-unit02_trial-7.sbatch
+sbatch 064_aug-none_test-unit02_trial-8.sbatch
+sbatch 064_aug-none_test-unit02_trial-9.sbatch
+
+sbatch 064_aug-none_test-unit03_trial-0.sbatch
+sbatch 064_aug-none_test-unit03_trial-1.sbatch
+sbatch 064_aug-none_test-unit03_trial-2.sbatch
+sbatch 064_aug-none_test-unit03_trial-3.sbatch
+sbatch 064_aug-none_test-unit03_trial-4.sbatch
+sbatch 064_aug-none_test-unit03_trial-5.sbatch
+sbatch 064_aug-none_test-unit03_trial-6.sbatch
+sbatch 064_aug-none_test-unit03_trial-7.sbatch
+sbatch 064_aug-none_test-unit03_trial-8.sbatch
+sbatch 064_aug-none_test-unit03_trial-9.sbatch
+
+sbatch 064_aug-none_test-unit05_trial-0.sbatch
+sbatch 064_aug-none_test-unit05_trial-1.sbatch
+sbatch 064_aug-none_test-unit05_trial-2.sbatch
+sbatch 064_aug-none_test-unit05_trial-3.sbatch
+sbatch 064_aug-none_test-unit05_trial-4.sbatch
+sbatch 064_aug-none_test-unit05_trial-5.sbatch
+sbatch 064_aug-none_test-unit05_trial-6.sbatch
+sbatch 064_aug-none_test-unit05_trial-7.sbatch
+sbatch 064_aug-none_test-unit05_trial-8.sbatch
+sbatch 064_aug-none_test-unit05_trial-9.sbatch
+
+sbatch 064_aug-none_test-unit07_trial-0.sbatch
+sbatch 064_aug-none_test-unit07_trial-1.sbatch
+sbatch 064_aug-none_test-unit07_trial-2.sbatch
+sbatch 064_aug-none_test-unit07_trial-3.sbatch
+sbatch 064_aug-none_test-unit07_trial-4.sbatch
+sbatch 064_aug-none_test-unit07_trial-5.sbatch
+sbatch 064_aug-none_test-unit07_trial-6.sbatch
+sbatch 064_aug-none_test-unit07_trial-7.sbatch
+sbatch 064_aug-none_test-unit07_trial-8.sbatch
+sbatch 064_aug-none_test-unit07_trial-9.sbatch
+
+sbatch 064_aug-none_test-unit10_trial-0.sbatch
+sbatch 064_aug-none_test-unit10_trial-1.sbatch
+sbatch 064_aug-none_test-unit10_trial-2.sbatch
+sbatch 064_aug-none_test-unit10_trial-3.sbatch
+sbatch 064_aug-none_test-unit10_trial-4.sbatch
+sbatch 064_aug-none_test-unit10_trial-5.sbatch
+sbatch 064_aug-none_test-unit10_trial-6.sbatch
+sbatch 064_aug-none_test-unit10_trial-7.sbatch
+sbatch 064_aug-none_test-unit10_trial-8.sbatch
+sbatch 064_aug-none_test-unit10_trial-9.sbatch
+
